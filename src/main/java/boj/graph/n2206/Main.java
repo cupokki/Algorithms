5f0f67ce678bada9f1fe39a88f7862c94ffc6a78
cupokki <nd1346@gmail.com>
2024-11-11 17:36:02 +0900

    메모리는 해결했으나 bfs를 모든 벽에 대해 실행하므로 시간복잡도가 큼. 1회 bfs를 실행하고 처음만난 벽인지 아닌지 체크할 로직 추가 필요

--- src/main/java/boj/graph/n2206/Main.java
+++ src/main/java/boj/graph/n2206/Main.java
@@ -11,7 +11,7 @@ public class Main {
     static Set<Integer> walls = new HashSet();
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {-1, 1, 0, 0};
-//    static List<List<Integer>> adjList = new ArrayList();
+    static List<List<Integer>> adjList = new ArrayList();
 
     static int n;
     static int m;
@@ -19,13 +19,10 @@ public class Main {
     static int toIndex(int x, int y) {
         return (x * m) + y;
     }
-
-    static int bfs(int n, int m, int root, int target) {
+    static int bfs(int n, int m, int root) {
         Queue<Integer> queue = new LinkedList<>();
-//        boolean[] visited = new boolean[n * m];
         Set<Integer> visited = new HashSet<>();
         int[] result = new int[n * m];
-//        visited[root] = true;
         visited.add(root);
         queue.add(root);
         result[root] = 0;
@@ -39,64 +36,63 @@ public class Main {
                 if (vx < 0 || vy < 0 || vx >= n || vy >= m)
                     continue;
                 int newNode = toIndex(vx, vy);
+//            for(int newNode : adjList.get(node)){
                 if (!visited.contains(newNode) && !walls.contains(newNode)) { // 벽이 아니라면 간다.
                     visited.add(newNode);
                     queue.add(newNode);
                     result[newNode] = result[node] + 1;
-                    if (newNode == target) {
-                        return result[newNode];
-                    }
+                    if(visited.contains(0) && visited.contains(n * m - 1))
+                        return result[0] + result[n * m - 1] + 1;
                 }
             }
         }
         return -1;
+
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-//        for (int i = 1; i <= n * m; i++) {
-//            adjList.add(new ArrayList<>());
-//        }
-//        map = new boolean[n * m];
         for (int i = 0; i < n; i++) {
             String str = sc.next();
             for (int j = 0; j < m; j++) {
                 if (str.charAt(j) == '1')
                     walls.add(toIndex(i, j));
-//                    map[toIndex(i, j)] = true;
             }
         }
-        // 첫번째도 벽으로 취급
-//        map[0] = true;
-
-        //간선추가
-//        for (int u = 0; u < n * m; u++) {
+//        //모든 노드 추가
+//        for(int i = 0; i < n * m; i++){
+//            adjList.add(new ArrayList<>());
+//        }
+//        //간선추가
+//        for(int u = 0; u < n * m; u++){
 //            int x = u / m;
 //            int y = u % m;
-//            for (int i = 0; i < 4; i++) {
+//            for(int i = 0; i < 4; i++) {
 //                int vx = x + dx[i];
 //                int vy = y + dy[i];
 //                if (vx < 0 || vy < 0 || vx >= n || vy >= m)
 //                    continue;
 //                int v = toIndex(vx, vy);
+//                if(walls.contains(v))
+//                    continue;
 //                adjList.get(u).add(v);
 //            }
 //        }
 
+
+
+
         // 모든 벽에서 출발해서 시작점과 타킷 두갈래로 찾아보자
+        // ver2. 그냥 벽에서 출발해서 result의 0과 n*m -1 을 더하자
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < n * m; i++) {
             if (walls.contains(i)) { // 벽이라면
-                int part1 = bfs(n, m, i, 0);
-                int part2 = bfs(n, m, i, n * m - 1);
-                if (part1 < 0 || part2 < 0) {
-                    continue;
-                }
-                min = Math.min(min, part1 + part2 + 1);
+                int result = bfs(n, m, i);
+                if(result != -1)
+                min = Math.min(min, result);
             }
-
         }
         if (min == Integer.MAX_VALUE)
             System.out.println(-1); //도달 불가시 -1
