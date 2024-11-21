db38c2a1515eeefc5c84dda41273ed0f6840a348
cupokki <nd1346@gmail.com>
2024-11-21 16:59:48 +0900

    solved : dijkstra, 01bfs

--- src/main/java/boj/shortestpath/n13549/Main.java
+++ src/main/java/boj/shortestpath/n13549/Main.java
@@ -1,6 +1,5 @@
 package boj.shortestpath.n13549;
 
-import java.io.IOException;
 import java.util.*;
 
 /**
@@ -17,65 +16,105 @@ import java.util.*;
  *      - 01 BFS를 구현하자
  *      - dp도 가능?
  */
-class Path {
+class State {
     int node;
     int dist;
 
-    public Path(int node, int dist) {
+    public State(int node, int dist) {
         this.node = node;
         this.dist = dist;
     }
 }
+
+//public class Main {
+//    static final int INF = Integer.MAX_VALUE;
+//    static final int MAX_POSITION = 100001;
+//    static boolean[][] graph;
+//    public static void main(String[] args) throws IOException {
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+//        int K = sc.nextInt();
+//        if(N >= K){
+//            System.out.println(N - K);
+//        }else{
+//            System.out.println(solve(N, K));
+//        }
+//    }
+//    static int solve(int root, int target) {
+//        int[] distance = new int[MAX_POSITION];
+//        Arrays.fill(distance, INF);
+////        PriorityQueue<Path> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.dist, b.dist));
+//        Queue<Path> pq = new LinkedList<>();
+//        pq.add(new Path(root, 0));
+//        distance[root] = 0;
+//
+//        while(!pq.isEmpty()) {
+//            Path p = pq.poll();
+//            int u = p.node;
+//            int curDist = p.dist;
+//            int v;
+//
+////            //pruning
+////            if(curDist > distance[u])
+////                continue;
+//
+//            // branchs
+//            v = u * 2;
+//            if(v < MAX_POSITION && distance[v] > curDist + 0) { //https://www.acmicpc.net/board/view/143652
+//                distance[v] = curDist + 0;
+//                pq.offer(new Path(v, distance[v]));
+//            }
+//            v = u - 1;
+//            if(v >= 0 && distance[v] > curDist + 1) {
+//                distance[v] = curDist + 1;
+//                pq.offer(new Path(v, distance[v]));
+//            }
+//            v = u + 1;
+//            if(v < MAX_POSITION && distance[v] > curDist + 1) {
+//                distance[v] = curDist + 1;
+//                pq.offer(new Path(v, distance[v]));
+//            }
+//
+//        }
+//
+//        return distance[target];
+//    }
+//}
+//
 public class Main {
-    static final int INF = Integer.MAX_VALUE;
-    static final int MAX_POSITION = 100001;
-    static boolean[][] graph;
-    public static void main(String[] args) throws IOException {
+    static final int MAX = 100000;
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int K = sc.nextInt();
-        if(N >= K){
-            System.out.println(N - K);
-        }else{
-            System.out.println(solve(N, K));
-        }
+        int result = N >= K? N - K : solve(N, K) ;
+        System.out.println(result);
     }
     static int solve(int root, int target) {
-        int[] distance = new int[MAX_POSITION];
-        Arrays.fill(distance, INF);
-        PriorityQueue<Path> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.dist, b.dist));
-        pq.add(new Path(root, 0));
-        distance[root] = 0;
+        int[] time = new int[MAX + 1];
+        Arrays.fill(time, Integer.MAX_VALUE);
+        Deque<State> dq = new LinkedList<>();
+        dq.offer(new State(root, 0));
+        time[root] = 0;
+        while(!dq.isEmpty()) {
+            State s = dq.pollFirst();
 
-        while(!pq.isEmpty()) {
-            Path p = pq.poll();
-            int u = p.node;
-            int curDist = p.dist;
-            int v;
-
-            //pruning
-            if(curDist > distance[u])
+            if(time[s.node] < s.dist)
                 continue;
 
-            // branchs
-            v = u * 2;
-            if(v < MAX_POSITION && distance[v] > curDist + 0) { //https://www.acmicpc.net/board/view/143652
-                distance[v] = curDist + 0;
-                pq.offer(new Path(v, distance[v]));
+            if(s.node * 2 <= MAX && time[s.node * 2] > s.dist + 0) {
+                time[s.node * 2] = s.dist;
+                dq.addFirst(new State(s.node * 2, s.dist));
             }
-            v = u - 1;
-            if(v >= 0 && distance[v] > curDist + 1) {
-                distance[v] = curDist + 1;
-                pq.offer(new Path(v, distance[v]));
+            if(s.node + 1 <= MAX && time[s.node + 1] > s.dist + 1) {
+                time[s.node + 1] = s.dist + 1;
+                dq.addLast(new State(s.node + 1, s.dist + 1));
             }
-            v = u + 1;
-            if(v < MAX_POSITION && distance[v] > curDist + 1) {
-                distance[v] = curDist + 1;
-                pq.offer(new Path(v, distance[v]));
+             if(s.node - 1 >= 0 && time[s.node - 1] > s.dist + 1) {
+                time[s.node - 1] = s.dist + 1;
+                dq.addLast(new State(s.node - 1, s.dist + 1));
             }
-
         }
-
-        return distance[target];
+        return time[target];
     }
 }
