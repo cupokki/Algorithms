3c5009bc1e0f7d9133110aa18341d3596708583e
cupokki <nd1346@gmail.com>
2024-12-02 22:35:16 +0900

    solve : 언더플로우 주의

--- src/main/java/boj/shortestpath/n11657/Main.java
+++ src/main/java/boj/shortestpath/n11657/Main.java
@@ -1,101 +1,85 @@
 package boj.shortestpath.n11657;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.List;
 import java.util.StringTokenizer;
 
 /**
  * https://www.acmicpc.net/problem/11657
  * 타임머신
- * N개의 도시. 한 도시에서 출발하여 다른 도시에 도착하는 버스 M개
- * A 시작도시,B 도착도시,C 버스를 타고 이동하는 시간(음수도 ok)
- * "1번 도시"에서 출발해서 나머지 도시로 가는 가장 빠른 시간을 구하라
- * <p>
- * 어떤 도시로 가는 과정에서 무한히 시간을 되돌릴 수 있다면 -1만 출력?
- * 다익스트라는 최소값만 찾아가는데, 방문한 노드를갔는데 값이 더 줄었다? 그럼 -1
+ * 한 경로에서 다른 점으로 가는 경로 중 가장 최단 경로를 찾아라
+ * 간선의 가중치는 음수일 수도 있으며, 경로 중간에 사이클이 존재할 수 있다.
+ * 만약 사이클이 존재한다면 -1을 반환하고, 아니라면 정점까지 최단거리를 순서대로 출력한다.
+ * 시작 노드는 항상 1이다.
+ * 다익스트라는 간선이 음수라면 사용 할 수 없다. -> 벨만 포드를 사용한다.
+ * 벨만 포드 알고리즘은 간선 리스트가 효율적이다.
  */
 public class Main {
+    static List<int[]> edges = new ArrayList<>(); // 간선리스트
     static final int INF = Integer.MAX_VALUE;
     static int N;
     static int M;
-    static int[][] graph;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 노드(도시) 개수
+        M = Integer.parseInt(st.nextToken()); // 간선 개수
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        //그래프 선언 및 초기화
-        graph = new int[N + 1][N + 1];
-        for (int[] arr : graph)
-            Arrays.fill(arr, INF);
-
-        //간선(버스) 입력
-        for (int i = 0; i < M; i++) {
+        // 간선추가.
+        for (int i = 1; i <= M; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken()); // a
-            int v = Integer.parseInt(st.nextToken()); // b
-            int w = Integer.parseInt(st.nextToken()); // c
-            graph[u][v] = w;
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            edges.add(new int[]{u, v, w}); //방향그래프
         }
-        br.close();
 
-        int[] result = dijkstra(1);
+        long[] result = bellmanFord(1); // 시작 도시 1번
 
         if (result == null) {
-            bw.write("-1\n");
+            bw.write(-1 + "\n");
         } else {
-            for (int i = 2; i <= N; i++)
-                bw.write(result[i] != INF ? result[i] + "\n" : "-1\n");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 2; i <= N; i++) {
+                sb.append(result[i] == INF ? -1 : result[i]).append("\n");
+            }
+            bw.write(sb.toString());
+
         }
         bw.flush();
         bw.close();
+        br.close();
     }
 
-    /**
-     * @param root
-     * @return 만약 루프 발생시 null출력
-     */
-    static int[] dijkstra(int root) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        int[] distance = new int[N + 1];
+    static long[] bellmanFord(int root) {
+        long[] distance = new long[N + 1]; // 노드1 까지 거리
         Arrays.fill(distance, INF);
         distance[root] = 0;
-        boolean[] visited = new boolean[N + 1];
-        visited[root] = true;
-        pq.offer(new int[]{root, 0});
 
-        while (!pq.isEmpty()) {
-            int[] state = pq.poll();
-            int u = state[0];
-            int curDist = state[1];
-
-            if (distance[u] < curDist) continue;
-
-            for (int v = 1; v <= N; v++) {
-                if(graph[u][v] == INF)
+        // (사이클이 아닌)최단경로는 최대 N - 1개의 간선을 포함
+        for (int i = 1; i < N; i++) { // 정점 개수 - 1 만큼 반복
+            for (int[] e : edges) {
+                int u = e[0], v = e[1], w = e[2];
+                if (distance[u] == INF)
                     continue;
-                //타임머신
-                // 이미 방문한 적이 있는데, 현거리가 음수고, 갈 거리도 음수라면
-                //
-                if (visited[v] && graph[u][v] < 0 && curDist < 0){
-                    return null;
-                }
-                if (distance[v] > graph[u][v] + curDist) {
-                    distance[v] = graph[u][v] + curDist;
-                    pq.offer(new int[]{v, distance[v]});
-                    visited[v] = true;
-                }
-
-
-
+                if (distance[u] + w < distance[v]) //간선완화
+                    distance[v] = distance[u] + w;
             }
         }
 
+        //사이클탐지
+        for (int[] e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            if (distance[u] == INF)
+                continue;
+            if (distance[u] + w < distance[v]) // 여기서 더 적어진다면 사이클이 존재하는 것
+                return null;
+
+        }
         return distance;
     }
 }
