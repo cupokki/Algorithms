3f8e2edbfc4f7a3c0213ff39d96a08cf1aad421a
cupokki <nd1346@gmail.com>
2024-12-12 00:59:15 +0900

    오답

--- src/main/java/boj/simulation/n12100/Main.java
+++ src/main/java/boj/simulation/n12100/Main.java
@@ -17,105 +17,89 @@ import java.util.StringTokenizer;
  */
 public class Main {
     static int[][] DIRECTIONS = {
-            {0, -1},// 위
-            {0, 1}, // 아래
-            {-1, 0},  // 왼쪽
-            {1, 0}   // 오른쪽
+            {-1, 0},// 위
+            {1, 0}, // 아래
+            {0, -1},  // 왼쪽
+            {0, 1}   // 오른쪽
     };
     static final int MAX_MOVE = 5;
     static int N;
     static int[][] board;
     static int maxNum;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         board = new int[N][N];
-
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
         dfs(MAX_MOVE);
         System.out.println(maxNum);
     }
 
-    // depth는 회차
     static void dfs(int depth) {
         if (depth == 0) {
             // 모든 방향 검토
             return;
         }
 
-        for (int d = 0; d < 4; d++) {
-            boolean changed = false;
-            int[][] origin = board.clone();
-            switch (d) { // 당길 방향
-                case 0:
-                    for (int i = 0; i < N; i++) // pull line
-                        changed = moveBlock(d, i, N - 1, false);
-                    break;
-                case 1:
-                    for (int i = 0; i < N; i++)// pull line
-                        changed = moveBlock(d, i, 0, false);
-                    break;
-                case 2:
-                    for (int i = 0; i < N; i++)// pull line
-                        changed = moveBlock(d, N - 1, i, false);
-                    break;
-                case 3:
-                    for (int i = 0; i < N; i++)// pull line
-                        changed = moveBlock(d, 0, i, false);
-                    break;
-                default:
-                    break;
-            }
-            if(!changed)
-                continue;
-            // TODO 이동을 못 시키고 넘어갈 수 도 있음
+        for (int dir = 0; dir < 4; dir++) {
+            int[][] original = board.clone();
+            moveBlocks(dir);
             dfs(depth - 1);
-            // restore
-            board = origin;
+            board = original;// restore
+
         }
     }
 
-    // 블록을 당겨오는 이미지다.
-    // 재귀적 호출이 별로 좋아보이진 않군..
-    static boolean moveBlock(int dir, int x, int y, boolean merged) {
-        // 현 위치의 블럭을 방향대로 이동시킨다.
-        int nx = x + DIRECTIONS[dir][0];
-        int ny = y + DIRECTIONS[dir][1];
+    static void moveBlocks(int dir) {
+        boolean moved = false;
+        if (dir == 0) { // 위로
+            for (int i = 0; i < N; i++)
+                moved = moveLine(dir, N - 1, i); // 아랫줄을 윗방향으로
+        } else if (dir == 1) {
+            for (int i = 0; i < N; i++)
+                moved = moveLine(dir, 0, i); // 윗 줄을 아래방향으로
+        } else if (dir == 2) {
+            for (int i = 0; i < N; i++)
+                moved = moveLine(dir, i, N - 1); // 오른쪽줄을 왼쪽으로
+        } else if (dir == 3) {
+            for (int i = 0; i < N; i++)
+                moved = moveLine(dir, i, 0); // 왼쪽줄을 오른쪽으로
+        }else {
+            return;
+        }
+        if(!moved)
+            moveBlocks(dir + 1);
+    }
 
+    // {0, 4, 2, 2} 배열을 <- 방향으로 민다면?
+    // 2에서 시작하는게 아니라 0에서 시작해서 수를 당겨와야한다.
+    static boolean moveLine(int dir, int r, int c) {
+        int nr = r, nc = c;
+        boolean moved = false;
         while (true) {
-            if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-                return false;
-
-            if (board[x][y] == 0) {
-                moveBlock(dir, nx, ny, false);
-                return true;
+            nr += DIRECTIONS[dir][0];
+            nc += DIRECTIONS[dir][1];
+            if (nr < 0 || nc < 0 || nr >= N || nc >= N) {
+                break;
             }
+            if (board[nr][nc] == 0)
+                continue;
 
-            if (!merged && board[x][y] == board[nx][ny]) { // 병합 후 이동
-                board[nx][ny] += board[x][y];//merge
-                maxNum = Math.max(maxNum, board[nx][ny]);
-                board[x][y] = 0;
-                moveBlock(dir, nx, ny, true); // 병합한채로 움직여라
-                return true; // 병합 후 현재의 것은 없어지므로
-            }
-            if (board[nx][ny] != 0)
-                return false;
-            //move
-            board[nx][ny] = board[x][y];
-            board[x][y] = 0;
-            moveBlock(dir, nx, ny, false);
+            board[r][c] += board[nr][nc]; // merge and pull
+            maxNum = Math.max(maxNum, board[r][c]);
+            if(board[r][c] != 0) moved = true;
+            // 이미 한번 머지했거나, 끝까지 땡긴것일 경우
+                r += DIRECTIONS[dir][0];
+                c += DIRECTIONS[dir][1];
 
-            x = nx;
-            y = ny;
-            nx += DIRECTIONS[dir][0];
-            ny += DIRECTIONS[dir][1];
+            board[nr][nc] = 0;
         }
+        return moved;
     }
-
 }
