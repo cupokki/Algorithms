a5280586d6927565b0b928d075a0528442d98cce
cupokki <nd1346@gmail.com>
2024-12-16 13:34:39 +0900

    solve

--- src/main/java/boj/simulation/n15686/Main.java
+++ src/main/java/boj/simulation/n15686/Main.java
@@ -15,12 +15,6 @@ import java.util.*;
  * 거리 = |r_1 - r_2| + |c_1 - c_2|
  * => 인접한것을 다루는 문제가 아님
  * M은 최대 선택가능한 치킨집의 개수.
- * <p>
- * 최소 도시치킨거리를 구하라
- * <p>
- * bfs -> M개 이하로 치킨집이 남을 수 있다.
- * -> dfs로 풀어야할 듯?
- * <p>
  * 집의 개수는 2 * N을 넘지않고, 최소 하나는 존재한다.
  */
 public class Main {
@@ -49,64 +43,60 @@ public class Main {
             }
         }
 
-        dfs(0, 0, 0);
+        solve(0, 0);
         System.out.println(min);
     }
-
-    static void dfs(int depth, int m, int cityChickenDistance) {
-        if (depth == houses.size()) {
-            // 최소 치킨거리 계산
-            min = Math.min(min, cityChickenDistance);
-            return;
-        }
-
-        int[] h = houses.get(depth);
-
-        if (choosen.size() == M) {
-            for (int[] c : choosen) {
-                int chickenDistance = Math.abs(h[0] - c[0]) + Math.abs(h[1] - c[1]);
-                // 집은 가게는 반드시 선택해야함
-                dfs(depth + 1, m + 1, cityChickenDistance + chickenDistance);
-
+    static void solve(int start, int size) {
+        if (size == M) {
+            int cityChickenDistance = 0;
+            for(int[] h : houses){
+                int minChickenDistance = Integer.MAX_VALUE;
+                for(int[] c : choosen){
+                    // 집들이 한 c만 고르게 하면 안된다.
+                    minChickenDistance = Math.min(Math.abs(h[0] - c[0]) + Math.abs(h[1] - c[1]), minChickenDistance);
+                }
+                cityChickenDistance += minChickenDistance;
             }
+            min = Math.min(cityChickenDistance, min);
             return;
         }
 
-        //(2 * N) ^ 13???
-        for (int ch = 0; ch < chickens.size(); ch++) { //
-            // 치킨집을 m개 선택했다면 무조건 선택한 치킨집 안
-            int[] c = chickens.get(ch);
-            int chickenDistance = Math.abs(h[0] - c[0]) + Math.abs(h[1] - c[1]);
-            // 집은 가게는 반드시 선택해야함
-            choosen.add(c);
-            dfs(depth + 1, m + 1, cityChickenDistance + chickenDistance);
-            choosen.remove(c);
-
+        // 중복을 허용하지않고 고름, 사실 choosen은 set을 사용해서 중복이 있어도 됨
+        // ㄴ 아님 각 분기의 크기를 줄여야함
+        for (int i = start; i < chickens.size(); i++) { // 조합 백트래킹
+            choosen.add(chickens.get(i));
+            solve(i + 1, size + 1);
+            choosen.remove(chickens.get(i));
         }
     }
 }
-//    static void dfs(int depth, int m, int cityChickenDistance) {
-//        if (depth == chickens.size()) {
-//            // 최소 치킨거리 계산
-//            if (m == M)
-//                min = Math.min(min, cityChickenDistance);
-//            return;
-//        }
+
+//static void dfs(int depth, int cityChickenDistance) {
+//    if (cityChickenDistance > min)
+//        return;
 //
-//        int[] c = chickens.get(depth);
-//        for (int hi = 0; hi < houses.size(); hi++) {
-//            int[] h = houses.get(hi);
+//    if (depth == houses.size()) { // 문제가 M을 13으로 설정한걸로 봐서 depth는 M임
+//        // 최소 치킨거리 계산
+//        min = Math.min(min, cityChickenDistance);
+//        return;
+//    }
+//
+//    // 집선택
+//    int[] h = houses.get(depth);
+//    if (choosen.size() == M) {
+//        for (int[] c : choosen) {
 //            int chickenDistance = Math.abs(h[0] - c[0]) + Math.abs(h[1] - c[1]);
-//
-//            dfs(depth + 1, m + 1, cityChickenDistance + chickenDistance);
-//
-//            dfs(depth + 1, m, cityChickenDistance);
-//
+//            dfs(depth + 1, cityChickenDistance + chickenDistance);
 //        }
+//        return;
+//    }
+//    for (int ch = 0; ch < chickens.size(); ch++) { // chichen.size()의 최댓값은 (n*2 - 2) 13은 M(선택된 치킨집의 최소)
+//        // 치킨집을 m개 선택했다면 무조건 선택한 치킨집 안
+//        int[] c = chickens.get(ch);
+//        int chickenDistance = Math.abs(h[0] - c[0]) + Math.abs(h[1] - c[1]);
+//        choosen.add(c);
+//        dfs(depth + 1, cityChickenDistance + chickenDistance);
+//        choosen.remove(c);
+//
 //    }
 //}
-
-// depth 마다 집을 고른다. -> 집은 반드시 치킨집을 고른다. <--------------------- O((2 * N) ^ 13)
-
-// depth 마다 치킨집을 고른다. -> 모든 치킨집은 집을 고르지 않는다.<---------------------- O((13)^3) // 고르거나/고르지않거나/폐업하거나?
-// chickens.size() 에서 M개를 선택하는 경우에서 모든 치킨집이 1회 이상 선택 되어야한다.
