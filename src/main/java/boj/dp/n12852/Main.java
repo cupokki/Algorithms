c02847fc73b5cc59f53922bd318d69eeafdb16af
cupokki <nd1346@gmail.com>
2025-01-09 00:35:57 +0900

    solve : dp

--- src/main/java/boj/dp/n12852/Main.java
+++ src/main/java/boj/dp/n12852/Main.java
@@ -17,48 +17,78 @@ import java.util.*;
  *
  *
  */
+//public class Main {
+//    static int min = Integer.MAX_VALUE;
+//    static int[] result = new int[1000000]; // 실제로 다 쓸 일 없음
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+//
+//        int[] list = new int[1000000];
+//        dfs(N, list, 0 );
+//
+//        System.out.println(min);
+//        for(int i = 0; i <= min; i++ ) {
+//            System.out.print(result[i] + " ");
+//        }
+//        System.out.println();
+//    }
+//
+//    static void dfs(int n, int[] list, int size) {
+//        list[size++] = n;
+//        if (size > min) {
+//            return;
+//        }
+//        if (n <= 0) {
+//            return;
+//        }
+//        if (n == 1) {
+//            if (min > size - 1) {
+//                min = size - 1;
+//                for (int i = 0; i <= min; i++) {
+//                    result[i] = list[i];
+//                }
+//            }
+//            return;
+//        }
+//
+//        if (n % 3 == 0) {
+//            dfs(n / 3, list, size);
+//        }
+//        if (n % 2 == 0) {
+//            dfs(n / 2, list, size);
+//        }
+//        dfs(n - 1, list, size);
+//    }
+//}
+//
+
 public class Main {
-    static int min = Integer.MAX_VALUE;
-    static int[] result = new int[1000000]; // 실제로 다 쓸 일 없음
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        int x = sc.nextInt();
 
-        int[] list = new int[1000000];
-        dfs(N, list, 0 );
+        int[] dp = new int[x + 1];
+        int[] result = new int[x + 1];
 
-        System.out.println(min);
-        for(int i = 0; i <= min; i++ ) {
-            System.out.print(result[i] + " ");
-        }
-        System.out.println();
-    }
-
-    static void dfs(int n, int[] list, int size) {
-        list[size++] = n;
-        if (size > min) {
-            return;
-        }
-        if (n <= 0) {
-            return;
-        }
-        if (n == 1) {
-            if (min > size - 1) {
-                min = size - 1;
-                for (int i = 0; i <= min; i++) {
-                    result[i] = list[i];
-                }
+        for (int i = 2; i <= x ; i++) {
+            dp[i] = dp[i - 1] + 1;
+            result[i] = i - 1;
+            if (i % 2 == 0 && dp[i / 2] + 1 < dp[i]) {
+                dp[i] = dp[i / 2] + 1;
+                result[i] = i / 2;
             }
-            return;
+            if (i % 3 == 0 && dp[i / 3] + 1 < dp[i]) {
+                dp[i] = dp[i / 3] + 1;
+                result[i] = i / 3;
+            }
         }
 
-        if (n % 3 == 0) {
-            dfs(n / 3, list, size);
+        System.out.println(dp[x]);
+
+        for (int i = x; i > 0;) {
+            System.out.print(i + " ");
+            i = result[i];
         }
-        if (n % 2 == 0) {
-            dfs(n / 2, list, size);
-        }
-        dfs(n - 1, list, size);
     }
 }
-
