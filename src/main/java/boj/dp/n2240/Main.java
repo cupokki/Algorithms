05c199a7345ff59b35a8847914d4f2fb2bb28c09
cupokki <nd1346@gmail.com>
2025-01-11 23:39:06 +0900

    solved : 다시 풀어보기

--- src/main/java/boj/dp/n2240/Main.java
+++ src/main/java/boj/dp/n2240/Main.java
@@ -16,8 +16,8 @@ import java.util.StringTokenizer;
  * 떨어지는 순간에 나무 아래 있으면 자두를 얻는다.
  * 자두는 t초간 떨어진다.
  * 사람은 최대 w회 움직인다.
+ *  - 안줌직여도 된다?
  *  - 움직임에 소요되는 시간은 없다.
- *  - 안움직여도 된다?
  * 사람의 초기 위치는 1이다.
  * 최대로 받을 수 있는 자두의 수는?
  *
@@ -29,38 +29,36 @@ public class Main {
         int T = Integer.parseInt(st.nextToken());
         int W = Integer.parseInt(st.nextToken());
 
-        int[][] plum = new int[T + 1][2];
+        int[] plum = new int[T + 1];
 
-        int tree1 = 0;
-        int tree2 = 0;
         for (int i = 1; i <= T; i++) {
-            if(br.readLine().equals("1")) {
-                plum[i][0] = 1;
-                tree1++ ;
-            } else {
-                plum[i][1] = 1;
-                tree2++;
+            plum[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[][] dp = new int[T + 1][W + 1];
+        int pos = 0;
+        for (int i = 1; i <= T; i++) {
+            for (int w = 0; w <= W; w++) {
+
+                pos = (w % 2) + 1;
+
+                if (pos == plum[i]) {
+                    dp[i][w] = dp[i - 1][w] + 1;
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+
+                if(w > 0) {
+                    dp[i][w] = Math.max(dp[i][w], dp[i -1][w - 1] + (plum[i] == pos ? 1 : 0));
+                }
             }
         }
 
-        // 자두 개수를 담는 dp
-//        int[][] dp = new int[t + 1][2];
-//        int pos = 0; // 사람의 초기 위치
-//        dp[1][0] = tree1;
-//        dp[1][1] = tree2;
-
-
-        int[][] dp = new int[T + 1][W + 1];
-        // 그림대로 빼나갈때 최대한으로 남은것을 선택한다.
-        // 빼나갈때는 어떤걸 고려해야하냐?
-        int[] left = new int[2];
-        int[] tree = new int[]{tree1, tree2};
-        for (int i = 1; i < T;i++) {
-
+        // 결과 출력
+        int result = 0;
+        for (int w = 0; w <= W; w++) {
+            result = Math.max(result, dp[T][w]);
         }
-
-
-        // 최대 자두 개수 출력
-        System.out.println();
+        System.out.println(result);
     }
 }
