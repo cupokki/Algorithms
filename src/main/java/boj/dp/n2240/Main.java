145980a902613c2e770bad6575c7847913f98954
cupokki <nd1346@gmail.com>
2025-01-12 23:39:18 +0900

    retry

--- src/main/java/boj/dp/n2240/Main.java
+++ src/main/java/boj/dp/n2240/Main.java
@@ -22,43 +22,48 @@ import java.util.StringTokenizer;
  * 최대로 받을 수 있는 자두의 수는?
  *
  */
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int T = Integer.parseInt(st.nextToken());
-        int W = Integer.parseInt(st.nextToken());
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int T = Integer.parseInt(st.nextToken());
+//        int W = Integer.parseInt(st.nextToken());
+//
+//        int[] plum = new int[T + 1];
+//
+//        for (int i = 1; i <= T; i++) {
+//            plum[i] = Integer.parseInt(br.readLine());
+//        }
+//
+//        int[][] dp = new int[T + 1][W + 1];
+//        int pos = 0;
+//        for (int i = 1; i <= T; i++) {
+//            for (int w = 0; w <= W; w++) {
+//
+//                pos = (w % 2) + 1;
+//
+//                if (pos == plum[i]) {
+//                    dp[i][w] = dp[i - 1][w] + 1;
+//                } else {
+//                    dp[i][w] = dp[i - 1][w];
+//                }
+//
+//                if(w > 0) {
+//                    dp[i][w] = Math.max(dp[i][w], dp[i -1][w - 1] + (plum[i] == pos ? 1 : 0));
+//                }
+//            }
+//        }
+//
+//        // 결과 출력
+//        int result = 0;
+//        for (int w = 0; w <= W; w++) {
+//            result = Math.max(result, dp[T][w]);
+//        }
+//        System.out.println(result);
+//    }
+//}
+public class Main{
+    public static void main(String[] args) {
 
-        int[] plum = new int[T + 1];
-
-        for (int i = 1; i <= T; i++) {
-            plum[i] = Integer.parseInt(br.readLine());
-        }
-
-        int[][] dp = new int[T + 1][W + 1];
-        int pos = 0;
-        for (int i = 1; i <= T; i++) {
-            for (int w = 0; w <= W; w++) {
-
-                pos = (w % 2) + 1;
-
-                if (pos == plum[i]) {
-                    dp[i][w] = dp[i - 1][w] + 1;
-                } else {
-                    dp[i][w] = dp[i - 1][w];
-                }
-
-                if(w > 0) {
-                    dp[i][w] = Math.max(dp[i][w], dp[i -1][w - 1] + (plum[i] == pos ? 1 : 0));
-                }
-            }
-        }
-
-        // 결과 출력
-        int result = 0;
-        for (int w = 0; w <= W; w++) {
-            result = Math.max(result, dp[T][w]);
-        }
-        System.out.println(result);
     }
 }
