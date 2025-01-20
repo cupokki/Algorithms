d4a8704252ec943d2ef6e48dd6f00194751c8771
cupokki <nd1346@gmail.com>
2025-01-20 13:47:02 +0900

    solved : 코드 개선

--- src/main/java/boj/dp/n9485/Main.java
+++ src/main/java/boj/dp/n9485/Main.java
@@ -34,7 +34,7 @@ public class Main {
                 stickers[1][i] = Integer.parseInt(st.nextToken());
             }
 
-            long[][] dp = new long[2][N + 1];
+            int[][] dp = new int[2][N + 1];
 
             dp[0][1] = stickers[0][1];
             dp[1][1] = stickers[1][1];
@@ -44,15 +44,8 @@ public class Main {
             //      2를 선택할 지
 
             for (int i = 2; i <= N; i++) { // i열 까지 스티커를 고려했을때
-                dp[0][i] = dp[0][i - 1];
-                dp[1][i] = dp[1][i - 1];
-
-                dp[0][i] = Math.max(dp[0][i], dp[1][i - 1] + stickers[0][i]);
-                dp[0][i] = Math.max(dp[0][i], dp[0][i - 2] + stickers[0][i]);
-                dp[1][i] = Math.max(dp[1][i], dp[0][i - 1] + stickers[1][i]);
-                dp[1][i] = Math.max(dp[1][i], dp[1][i - 2] + stickers[1][i]);
-
-
+                dp[0][i] = Math.max(dp[1][i - 2] + stickers[0][i], dp[1][i - 1] + stickers[0][i]);
+                dp[1][i] = Math.max(dp[0][i - 2] + stickers[1][i], dp[0][i - 1] + stickers[1][i]);
             }
             System.out.println(Math.max(dp[0][N], dp[1][N]));
         }
