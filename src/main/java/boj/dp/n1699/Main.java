9179c89f126777bb89b632b3eeb739413fed29b0
cupokki <nd1346@gmail.com>
2025-02-11 23:37:53 +0900

    temp

--- src/main/java/boj/dp/n1699/Main.java
+++ src/main/java/boj/dp/n1699/Main.java
@@ -2,8 +2,6 @@ package boj.dp.n1699;
 
 import java.util.Scanner;
 
-import static java.lang.Math.sqrt;
-
 /**
  * 제곱수의 합
  * https://www.acmicpc.net/problem/1699
@@ -15,17 +13,14 @@ import static java.lang.Math.sqrt;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 입력은 10만이하의 "자연수"
+        int N = sc.nextInt();
+
         int[] dp = new int[N + 1];
 
         for (int i = 1; i <= N; i++) {
             dp[i] = i; // 기본적으로 i(1의 제곱을 을 i번 더했을 때)로 초기화
             for (int j = 1; j * j <= i; j++) {
-                if (j * j == i) { // j가 i의 제곱근이라면
-                    dp[i] = 1; // j의 제곱 1항
-                } else {
-                    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
-                }
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1); // i번 더한것, 또는 지금까지 최솟값과 비교했을 때, j의 제곱으로 i에서 도달가능하면 dp[i- j * j]에서 1추가한 것이 dp[i]이다.
             }
         }
         System.out.println(dp[N]);
