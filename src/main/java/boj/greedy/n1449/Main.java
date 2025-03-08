6c9901ed9544bdca0aa6ce28afdfbea268e9ad9e
cupokki <nd1346@gmail.com>
2025-03-09 00:36:34 +0900

    solved

--- src/main/java/boj/greedy/n1449/Main.java
+++ src/main/java/boj/greedy/n1449/Main.java
@@ -1,9 +1,11 @@
-package boj.n1449;
+package boj.greedy.n1449;
 
 import java.io.*;
 import java.util.*;
 
 /**
+ * 수리공 항승
+ * https://www.acmicpc.net/problem/1449
  * 파이프는 가장 왼쪽에서 정수만큼 떨어진 거리만 물이 샌다.
  * 길이가 L인 테이프 무한개 테이프로 물을 막는다.
  * 물을 막을때, 좌우 0.5만큼 간격을 줘야 물이 다시 안 샌다. -> 지점당 길이 1씩 필요, 테이프는 자를 수 없다.
