3429fa1fb1ced369e9efeec8f418f5543e68f584
cupokki <nd1346@gmail.com>
2025-03-16 17:52:01 +0900

    solved

--- src/main/java/boj/implement/n15685/Main.java
+++ src/main/java/boj/implement/n15685/Main.java
@@ -1,4 +1,4 @@
-package boj.n15685;
+package boj.implement.n15685;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -23,16 +23,7 @@ import java.util.*;
  *      1: y좌표가 감소하는 방향 (↑)
  *      2: x좌표가 감소하는 방향 (←)
  *      3: y좌표가 증가하는 방향 (↓)
- *
- *
- *  상대적인 방향만 기록하면? 스택같은 구조로 해결할 수 있지않을까?
- *  0 : 스택서 선분을 꺼낸다. 끝점을 계산한다. 끝점을 시작점으로 바꾼 걸, 방향을 수정(+1 * 스택깊이)해서 넣는다.
- *  1 : 스택서 선분을 꺼낸다. 끝점을 계산한다. 끝점을 시작점으로 바꾼다.
- *
- *  선분을 꺼낸다.
- *  꺼내진 좌표는 시작점이 된다.
- *  꺼내진 좌표와 방향을 기반으로 새로운 끝점을 만든다.
- *  끝점을 스택에 담는다.
+
  */
 public class Main {
     static int[] dx = {1, 0, -1, 0};
@@ -41,42 +32,41 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         boolean[][] map = new boolean[101][101];
-        Stack<int[]> stack = new Stack<>();
 
-        for (int i = 0; i < N; i++) {
+        for (int n = 0; n < N; n++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             int d = Integer.parseInt(st.nextToken());
             int G = Integer.parseInt(st.nextToken());
-            map[x][y] = true;
-            Queue<Integer> directions = new LinkedList<>();
-            directions.offer(d);
-            // 0세대
-            int gen = 1; // 현재
-
-            while (G > gen) { // == directions.size() == gen
-                int curD = (directions.poll() - gen + 4) % 4;
-                x += dx[curD];
-                y += dy[curD];
-                map[x][y] = true;
-                directions.offer(curD);
+            List<Integer> dirs = new ArrayList<>();
+            dirs.add(d);
+            int gen = 1;
+            while (G >= gen) {
+                int len = dirs.size();
+                for (int i = len - 1; i >=0; i--) {
+                    dirs.add((dirs.get(i) + 1) % 4);
+                }
                 gen++;
             }
+            drawByDirections(map, x, y, dirs);
         }
 
         System.out.println(calcSquareUnitCnt(map));
     }
-
-    static void drawLine(boolean[][] map, int x, int y, int d) {
-        map[x + dx[d]][y + dy[d]] = true;
+    static void drawByDirections(boolean[][] map, int x, int y, List<Integer> dirs) {
+        map[x][y] = true;
+        for (int d : dirs) {
+            x += dx[d];
+            y += dy[d];
+            map[x][y] = true;
+        }
     }
-
     static int calcSquareUnitCnt(boolean[][] map) {
         int cnt = 0;
-        for (int i = 1; i < 100; i++) {
-            for (int j = 1; j < 100; j++)
-                if (map[i - 1][j - 1] || map[i - 1][j] || map[i][j - 1] || map[i][j])
+        for (int i = 1; i <= 100; i++) {
+            for (int j = 1; j <= 100; j++)
+                if (map[i - 1][j - 1] && map[i - 1][j] && map[i][j - 1] && map[i][j])
                     cnt++;
         }
         return cnt;
