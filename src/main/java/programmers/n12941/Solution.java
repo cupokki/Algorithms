2eea03f4e8849956d9671af92221665c4f8b4ca5
cupokki <nd1346@gmail.com>
2025-08-06 23:56:39 +0900

    temp

---
+++ src/main/java/programmers/n12941/Solution.java
@@ -0,0 +1,12 @@
+package programmers.n12941;
+
+public class Solution {
+    public static int solution(int[] A, int[] B) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 4, 2}, new int[]{5, 4, 4}) == 29);
+        System.out.println(solution(new int[]{1, 2}, new int[]{3, 4}) == 10);
+    }
+}
