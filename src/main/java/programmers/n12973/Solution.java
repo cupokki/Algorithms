b5fd87ad60e3eab12c50d9fccd926b781b0ae938
cupokki <nd1346@gmail.com>
2025-08-11 23:47:23 +0900

    solved

--- src/main/java/programmers/n12973/Solution.java
+++ src/main/java/programmers/n12973/Solution.java
@@ -1,21 +1,21 @@
 package programmers.n12973;
 
+import java.util.Stack;
+
 public class Solution {
     static int solution(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        int idx = 1;
-        while(idx < sb.length()) {
-            if(sb.charAt(idx - 1) == sb.charAt(idx)) {
-                sb.deleteCharAt(idx - 1);
-                sb.deleteCharAt(idx - 1);
-                idx = 1;
-            }
-            else {
-                idx++;
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            if (!stack.isEmpty() && stack.peek() == s.charAt(i)) {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
             }
         }
 
-        return sb.isEmpty() ? 1 : 0;
+
+        return stack.isEmpty() ? 1 : 0;
     }
     public static void main(String[] args) {
         System.out.println(solution("baabaa"));
