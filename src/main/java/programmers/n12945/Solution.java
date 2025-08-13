62aedaeb80c21a13c065a840b3af9761cdc275c3
cupokki <nd1346@gmail.com>
2025-08-13 20:58:48 +0900

    solved : tabulation

---
+++ src/main/java/programmers/n12945/Solution.java
@@ -0,0 +1,16 @@
+package programmers.n12945;
+
+import java.util.Arrays;
+
+public class Solution {
+    static final int MOD = 1_234_567;
+     static public int solution(int n) {
+         int[] dp = new int[n + 1];
+         dp[1] = 1;
+         for (int i = 2; i <= n; i++) {
+             dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+         }
+         return dp[n];
+     }
+
+}
