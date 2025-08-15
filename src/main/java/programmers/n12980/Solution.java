36232efbc5e3d358c7fa79d5c935e63157d5d15a
cupokki <nd1346@gmail.com>
2025-08-15 23:30:40 +0900

    fail: out of memory

--- src/main/java/programmers/n12980/Solution.java
+++ src/main/java/programmers/n12980/Solution.java
@@ -4,26 +4,36 @@ import java.util.*;
 
 public class Solution {
     static int memo[];
+
     static int solution(int n) {
         // k칸 앞점프, k만큼 에너지
         // (현재온 거리)*2 위치로 순간이동, 소모없음
         // 처음 위치 0에서 n까지의 최소 에너지 소모량 k?
 
         memo = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            memo[i] = i;
-        }
+        Arrays.fill(memo, -1);
+        memo[0] = 0;
+        memo[1] = 1;
 
-        return Math.min(move(n - 1) + 1, n != 0 ? move(n / 2) : Integer.MAX_VALUE);
+        return move(n);
     }
 
     static int move(int n) {
         if (n == 0) {
-            return memo[1];
+            return memo[0];
         }
-        memo[n] = Math.min(move(n - 1) + 1, n != 0 ? move(n / 2) : Integer.MAX_VALUE);
-        return memo[n];
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+        int temp = move(n - 1) + 1;
+
+        if (n % 2 == 0) {
+            temp = move(n / 2);
+        }
+        memo[n] = temp;
+        return temp;
     }
+
     public static void main(String[] args) {
         System.out.println(solution(5));
         System.out.println(solution(6));
