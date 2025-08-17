847fd0b7f4244902b40a4ffc0261450360c58ae2
cupokki <nd1346@gmail.com>
2025-08-17 23:58:23 +0900

    temp

--- src/main/java/programmers/n42885/Solution.java
+++ src/main/java/programmers/n42885/Solution.java
@@ -1,33 +1,37 @@
 package programmers.n42885;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Solution {
     // 보트를 타고 무인도 탈출
     // 최대 2명씩, 무게제한 있음
     // 보트 이용 최소의 수를 구하여라
-    static int[] p;
-    static int l;
-    static boolean[] visited;
-    static int min = Integer.MAX_VALUE;
+    // 완전탐색으론 시간 초과 예상 불가
+    // 백트래킹역시 한 레이어에서
     static int solution(int[] people, int limit) {
-        p = people;
-        l = limit;
-        visited = new boolean[people.length];
-        backtracking(0, 0);
-        return min;
-    }
-    static void backtracking(int rescued, int cnt) {
+        List<Integer> sorted = Arrays.stream(people)
+                .boxed()
+                .sorted(Collections.reverseOrder())
+                .collect(Collectors.toList());
 
-        if(rescued == p.length) {
-            min = Math.min(min, cnt);
-        }
-
-        for (int i = 0; i < p.length; i++) {
-            if (!visited[i]) {
-               visited[i] = true;
-               backtracking(0, 0);
-               visited[i] = false;
+        int result = 0;
+        while(!sorted.isEmpty()) {
+            int remain = limit;
+            List<Integer> chosen = new ArrayList<>();
+            for (int w : sorted) {
+                if (remain >= w) {
+                    remain -=w;
+                    chosen.add(w);
+                }
             }
+            for(int w : chosen) {
+                sorted.removeIf(i-> i.equals(w));
+            }
+            result++;
         }
+
+        return result;
     }
 
     public static void main(String[] args) {
