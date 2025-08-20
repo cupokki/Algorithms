310bda60d59d0c77144e8c39a2819ec817c7fdbd
cupokki <nd1346@gmail.com>
2025-08-20 23:44:11 +0900

    temp

--- src/main/java/programmers/n138476/Solution.java
+++ src/main/java/programmers/n138476/Solution.java
@@ -1,31 +1,9 @@
 package programmers.n138476;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Solution {
     static int solution(int k, int[] tangerine) {
-        Arrays.sort(tangerine);
-        int max = tangerine[tangerine.length + 1];
-        int[] size = new int[max + 1];
-        for (int t : tangerine) {
-            size[t]++;
-        }
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 1; i <= max; i++) {
-            if (size[i] != 0) {
-                map.put(i, size[i]);
-            }
-        }
-        AtomicInteger cnt = new AtomicInteger(0);
-        return map.keySet().stream().sorted(Collections.reverseOrder())
-                .filter(i -> cnt.incrementAndGet() < k)
-                .toList()
-                .size();
-
+        int tSize = tangerine.length;
+        return 0;
     }
     public static void main(String[] args) {
         System.out.println(solution(6, new int[]{1, 3, 2, 5, 4, 5, 2, 3}));
