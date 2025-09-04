0eaac3ca3d7916ba0a2be028c3d2632818adafe4
cupokki <nd1346@gmail.com>
2025-09-04 23:52:59 +0900

    temp

--- src/main/java/programmers/n87390/Solution.java
+++ src/main/java/programmers/n87390/Solution.java
@@ -2,33 +2,31 @@ package programmers.n87390;
 
 public class Solution {
     public static int[] solution(int n, long left, long right) {
-        int[][] arr = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            int idx = i + 1;
-            for (int j = 0; j < n; j++) {
-                if (j > idx - 1) idx++;
-                arr[i][j] = idx;
-            }
-        }
+
         int si = (int)left / n;
         int sj = (int)left % n;
         int ei = (int)right / n;
         int ej = (int)right % n;
+
+        int[] arr = new int[(ei - si + 1) * n];
+        for (int i = si; i <= ei; i++) {
+            int idx = i + 1;
+            for (int j = 0; j < n; j++) {
+                if (j > idx) idx++;
+                arr[i * n + j] = idx;
+            }
+
+        }
+
         int range = (int)(right - left) + 1;
         int[] answer = new int[range];
         int idx = 0;
-        for (int j = sj; j < n; j++) {
-            answer[idx++] = arr[si][j];
+        while(arr[idx] != 0) {
+            idx++;
         }
-        for (int i = si + 1; i < ei; i++) {
-            for (int j = 0; j < n; j++) {
-                answer[idx++] = arr[i][j];
-            }
+        for (int i = 0 ; i < range; i++) {
+            answer[i] = arr[idx + i];
         }
-        for (int j = 0; j <= ej; j++) {
-            answer[idx++] = arr[ei][j];
-        }
-
 //        System.out.print("[");
 //        for (int num : answer) {
 //            System.out.print(num + ", ");
