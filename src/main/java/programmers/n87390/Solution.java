f6bbbb08dfdbbd06a3017af6ef66d1d441f91791
cupokki <nd1346@gmail.com>
2025-09-05 23:29:41 +0900

    solved

--- src/main/java/programmers/n87390/Solution.java
+++ src/main/java/programmers/n87390/Solution.java
@@ -2,26 +2,14 @@ package programmers.n87390;
 
 public class Solution {
     public static int[] solution(int n, long left, long right) {
+        int len = (int)(right - left + 1);
+        int[] answer = new int[len];
 
-        int si = (int)left / n;
-        int sj = (int)left % n;
-        int ei = (int)right / n;
-//        int ej = (int)right % n;
-
-        int[] arr = new int[(ei - si + 1) * n];
-        for (int i = 0; i < ei - si + 1; i++) {
-            int idx = si + i + 1;
-            for (int j = 0; j < n; j++) {
-                if (j + 1 > idx) idx++;
-                arr[i * n + j] = idx;
-            }
-        }
-
-        int range = (int)(right - left) + 1;
-        int[] answer = new int[range];
-        // 첫행에서 ei 부터 n 열까지
-        for (int i = 0; i < range; i++) {
-            answer[i] = arr[sj + i];
+        for (int i = 0; i < len; i++) {
+            long idx = left + i;
+            int row = (int)(idx / n);
+            int col = (int)(idx % n);
+            answer[i] = Math.max(row, col) + 1;
         }
 
         return answer;
