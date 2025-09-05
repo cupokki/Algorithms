ac873d06f8c921f88c830c84ac94e1b5c5170cb8
cupokki <nd1346@gmail.com>
2025-09-05 23:19:21 +0900

    temp

--- src/main/java/programmers/n87390/Solution.java
+++ src/main/java/programmers/n87390/Solution.java
@@ -6,32 +6,24 @@ public class Solution {
         int si = (int)left / n;
         int sj = (int)left % n;
         int ei = (int)right / n;
-        int ej = (int)right % n;
+//        int ej = (int)right % n;
 
         int[] arr = new int[(ei - si + 1) * n];
-        for (int i = si; i <= ei; i++) {
-            int idx = i + 1;
+        for (int i = 0; i < ei - si + 1; i++) {
+            int idx = si + i + 1;
             for (int j = 0; j < n; j++) {
-                if (j > idx) idx++;
+                if (j + 1 > idx) idx++;
                 arr[i * n + j] = idx;
             }
-
         }
 
         int range = (int)(right - left) + 1;
         int[] answer = new int[range];
-        int idx = 0;
-        while(arr[idx] != 0) {
-            idx++;
+        // 첫행에서 ei 부터 n 열까지
+        for (int i = 0; i < range; i++) {
+            answer[i] = arr[sj + i];
         }
-        for (int i = 0 ; i < range; i++) {
-            answer[i] = arr[idx + i];
-        }
-//        System.out.print("[");
-//        for (int num : answer) {
-//            System.out.print(num + ", ");
-//        }
-//        System.out.println("]");
+
         return answer;
     }
     public static void main(String[] args) {
