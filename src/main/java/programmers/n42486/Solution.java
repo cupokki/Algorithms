3bfe5c2c0a84fcbfa5e29f0ced9070152578e7f6
cupokki <nd1346@gmail.com>
2025-09-11 22:49:14 +0900

    solved

--- src/main/java/programmers/n42486/Solution.java
+++ src/main/java/programmers/n42486/Solution.java
@@ -1,51 +1,40 @@
 package programmers.n42486;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Solution {
     // 순서대로 앞에것이 배포되어야 배포가능
     public static int[] solution(int[] progress, int[] speeds) {
+        List<Integer> result = new ArrayList<>();
         int len = progress.length;
-        int deployed = 0;
-        List<Integer> list = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
-        while(deployed != len) {
-            for (int i = 0; i < len; i++) {
-                if (progress[i] < 0) {
-                    continue;
-                }
-                if (progress[i] < 100) progress[i] += speeds[i];
-                if (progress[i] >= 100) {
-
-                    if (i == 0) {
-                        stack.push(i);
-                        deployed++;
-                        progress[i] = Integer.MIN_VALUE;
-                    }
-
-                    if (!stack.isEmpty() && stack.peek() == i - 1) {
-                        stack.push(i);
-                        deployed++;
-                        progress[i] = Integer.MIN_VALUE;
-                    }
-                }
+        int idx = 0;
+        while (idx != len) {
+            for (int i = idx; i < len; i++) {
+                progress[i] += speeds[i];
             }
-            if (!stack.isEmpty()) {
-                list.add(stack.size());
+            int cnt = 0;
+            for (int i = idx; i < len; i++) {
+                if (progress[i] >= 100 ) {
+                    cnt++;
+                }else {
+                    break;
+                }
+
+            }
+            if (cnt != 0) {
+                result.add(cnt);
+                idx += cnt;
             }
         }
 
-        int[] result = list.stream().mapToInt(n->n).toArray();
-        return result;
+        return result.stream().mapToInt(i->i).toArray();
     }
 
     public static void main(String[] args) {
-        solution(
-                new int[]{93, 30, 55},
-                new int[]{1, 30, 5}
-                );
+//        solution(
+//                new int[]{93, 30, 55},
+//                new int[]{1, 30, 5}
+//                );
 
         solution(
                 new int[]{95, 90, 99, 99, 80, 99},
