1ec4485d8c0886d5acf576b2bf5f481cb7bc6360
cupokki <nd1346@gmail.com>
2025-09-12 22:26:58 +0900

    solved

--- src/main/java/programmers/n17680/Solution.java
+++ src/main/java/programmers/n17680/Solution.java
@@ -5,16 +5,22 @@ import java.util.Queue;
 
 public class Solution {
     public static int solution(int cacheSize, String[] cities) {
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
         Queue<String> cache = new LinkedList<>();
+
         int answer = 0;
         for (String city : cities) {
-            if(cache.contains(city.toUpperCase())){ // hit
+            String query = city.toUpperCase();
+            if(cache.contains(query)){ // hit
                 answer += 1;
+                cache.remove(query);
             } else { // miss
                 if (cache.size() == cacheSize) cache.poll();
-                cache.offer(city.toUpperCase());
                 answer += 5;
             }
+            cache.offer(query);
         }
         return answer; // 총 실행시간
     }
