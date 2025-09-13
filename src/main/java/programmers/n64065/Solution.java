a37b670c4ff21f240255179eee39a31f047065b7
cupokki <nd1346@gmail.com>
2025-09-13 23:57:42 +0900

    solved

--- src/main/java/programmers/n64065/Solution.java
+++ src/main/java/programmers/n64065/Solution.java
@@ -1,7 +1,6 @@
 package programmers.n64065;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Solution {
     /*
@@ -10,17 +9,24 @@ public class Solution {
      */
     public static int[] solution(String s) {
         String[] row = s.split("},\\s*\\{");
-        Queue<Integer> queue = new LinkedList<>();
+        List<int[]> list = new ArrayList<>();
         for (String str : row) {
             String numbers = str.replaceAll("[/{/}]", "");
-            for (String num : numbers.split(",")) {
-                int n = Integer.parseInt(num);
-                if (!queue.contains(n)) {
-                    queue.offer(n);
+            list.add(Arrays.stream(numbers.split(",")).mapToInt(Integer::parseInt).toArray());
+        }
+
+        list.sort((a,b) -> a.length - b.length);
+
+        List<Integer> result = new ArrayList<>();
+        for (int[] arr : list) {
+            for (int n : arr) {
+                if (!result.contains(n)){
+                    result.add(n);
                 }
             }
         }
-        return queue.stream().mapToInt(Integer::intValue).toArray();
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
     public static void main(String[] args) {
         solution("{{2},{2,1},{2,1,3},{2,1,3,4}}");
