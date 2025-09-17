90d7df96706f994c313f2e34c2d80d3768fdccd5
cupokki <nd1346@gmail.com>
2025-09-17 23:49:22 +0900

    solved

--- src/main/java/programmers/n132265/Solution.java
+++ src/main/java/programmers/n132265/Solution.java
@@ -1,25 +1,31 @@
 package programmers.n132265;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Solution {
     public static int solution(int[] topping) {
         int len = topping.length;
         int answer = 0;
-        Queue<Integer> left = new LinkedList<>();
-        Queue<Integer> right = new LinkedList<>(Arrays.stream(topping).boxed().collect(Collectors.toList()));
-        for (int c = 0; c < len; c++) {
-            left.offer(right.poll());
-            int lSize = (int) left.stream().distinct().count();
-            int rSize = (int) right.stream().distinct().count();
-            if (lSize == rSize) {
+
+        Set<Integer> left = new HashSet<>();
+        Set<Integer> right = new HashSet<>();
+        int[] rightCount = new int[len];
+
+        for (int i = len - 1; i >= 0; i--) {
+            right.add(topping[i]);
+            rightCount[i] = right.size();
+        }
+
+        for (int i = 0; i < len - 1; i++) {
+            left.add(topping[i]);
+            if (left.size() == rightCount[i + 1]) {
                 answer++;
             }
-
         }
+
         return answer;
     }
+
 
     public static void main(String[] args) {
         System.out.println(solution(new int[]{1, 2, 1, 3, 1, 4, 1, 2}));
