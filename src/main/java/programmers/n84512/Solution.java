7813febc2d7800134f698f91a1ac27dde04fd7d5
cupokki <nd1346@gmail.com>
2025-09-20 23:58:45 +0900

    temp

---
+++ src/main/java/programmers/n84512/Solution.java
@@ -0,0 +1,41 @@
+package programmers.n84512;
+
+public class Solution {
+    /*
+    ____A
+    ___AA
+    __AAA
+    _AAAA
+    AAAAA
+    AAAAE
+    AAAAI
+    AAAAO
+    AAAAU
+    AAAEA
+    ..
+    AUUUU
+    ____U
+    ___UA
+    __UAA
+    _UAAA
+    UAAAA
+    UAAAE
+    UAAAI
+    UAAA
+    ___UE
+    ___UI
+    ___UO
+    ___UU
+
+     */
+    public static int solution(String word) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("AAAAE"));
+        System.out.println(solution("AAAE"));
+        System.out.println(solution("I"));
+        System.out.println(solution("EIO"));
+    }
+}
