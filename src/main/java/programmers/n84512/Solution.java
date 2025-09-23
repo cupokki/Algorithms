65dcd34eb5f6f2a0345e50fa29960bf0e2556401
cupokki <nd1346@gmail.com>
2025-09-23 23:17:44 +0900

    solved

--- src/main/java/programmers/n84512/Solution.java
+++ src/main/java/programmers/n84512/Solution.java
@@ -1,36 +1,41 @@
 package programmers.n84512;
 
 public class Solution {
-    /*
-    ____A
-    ___AA
-    __AAA
-    _AAAA
-    AAAAA
-    AAAAE
-    AAAAI
-    AAAAO
-    AAAAU
-    AAAEA
-    ..
-    AUUUU
-    ____U
-    ___UA
-    __UAA
-    _UAAA
-    UAAAA
-    UAAAE
-    UAAAI
-    UAAA
-    ___UE
-    ___UI
-    ___UO
-    ___UU
 
-     */
+    static boolean done = false;
+    static int answer = 0;
+    static int count = 0;
+    static char[] cSet = {'A', 'E', 'I', 'O', 'U'};
+
+
     public static int solution(String word) {
-        return 0;
+//        answer = 0;
+//        count = 0;
+//        done = false;
+        backtracking("", word);
+        return answer;
     }
+
+    public static void backtracking(String current, String word) {
+        if (done) return;
+
+        // 단어 하나 생성했을 때 count 증가
+        if (!current.isEmpty()) count++;
+
+        if (current.equals(word)) {
+            answer = count;
+            done = true;
+            return;
+        }
+
+        if (current.length() == 5) return;
+
+        for (int i = 0; i < cSet.length; i++) {
+            backtracking(current + cSet[i], word);
+        }
+    }
+
+
 
     public static void main(String[] args) {
         System.out.println(solution("AAAAE"));
