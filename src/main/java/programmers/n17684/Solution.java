67cae8b5a2855b23970b96098f2a041787cadf79
cupokki <nd1346@gmail.com>
2025-09-27 23:20:34 +0900

    solved

---
+++ src/main/java/programmers/n17684/Solution.java
@@ -0,0 +1,49 @@
+package programmers.n17684;
+
+import java.util.*;
+
+public class Solution {
+    public static int[] solution(String msg) {
+        List<Integer> result = new ArrayList<>();
+        Map<String, Integer> map = new HashMap();
+
+        int index = 1;
+        for (char c = 'A'; c <= 'Z'; c++) {
+            map.put(c+"", index);
+            index++;
+        }
+        int len = msg.length();
+        msg += " ";
+        for (int i = 0; i < len;) {
+            String prev = msg.charAt(i) + "";
+            int j = i + 1;
+            String current = msg.substring(i, j);
+            while (map.containsKey(current)) {
+                prev = current;
+                j++;
+                current = msg.substring(i, j);
+            }
+            result.add(map.get(prev));
+            map.put(current, index++);
+            i += prev.length();
+        }
+
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+//        Arrays.stream(solution("KAKAO")).forEach(i -> {
+//            System.out.print(i + " ");
+//        });
+//        System.out.println();
+        Arrays.stream(solution("TOBEORNOTTOBEORTOBEORNOT")).forEach(i -> {
+            System.out.print(i + " ");
+        });
+        System.out.println();
+//        Arrays.stream(solution("ABABABABABABABAB")).forEach(i -> {
+//            System.out.print(i + " ");
+//        });
+//        System.out.println();
+    }
+}
