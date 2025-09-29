0d050e30855a8cfdf155cd01bb0ae36417ef1f6b
cupokki <nd1346@gmail.com>
2025-09-30 00:18:52 +0900

    temp

--- src/main/java/programmers/n12941/Solution.java
+++ src/main/java/programmers/n12941/Solution.java
@@ -3,33 +3,34 @@ package programmers.n12941;
 import java.util.*;
 
 public class Solution {
+    static int min = Integer.MAX_VALUE;
     public static int solution(int[] A, int[] B) {
-//        int size = A.length;
-//        boolean[] visited = new boolean[size];
-//        int[] result = new int[1];
-//        result[0] = Integer.MAX_VALUE;
-//        dfs(0, A, B, 0, 0, result);
-//        return result[0];
-        Arrays.sort(A);
-        Arrays.sort(B);
         int size = A.length;
-        int sum = 0;
-        for (int i = 0; i < size; i++) {
-            sum += A[i] * B[size - 1 - i];
-        }
-        return sum;
-    }
-//    static void dfs(int sum, int[] A, int[] B, int depth, int start, int[] result) {
+        boolean[] visited = new boolean[size];
+        dfs(visited, A, B, 0, 0);
+        return min;
+//        Arrays.sort(A);
+//        Arrays.sort(B);
 //        int size = A.length;
-//        if (depth == 3) {
-//            result[0] = Math.min(result[0], sum);
-//            return;
+//        int sum = 0;
+//        for (int i = 0; i < size; i++) {
+//            sum += A[i] * B[size - 1 - i];
 //        }
-//
-//        for (int i = start; i < size; i++) {
-//            dfs(sum + A[depth] * B[i], A, B, i + 1 , i + 1, result);
-//        }
-//    }
+//        return sum;
+    }
+    static void dfs(boolean[] visited, int[] A, int[] B, int depth, int sum) {
+        if (depth == A.length) {
+            min = Math.min(min, sum);
+            return;
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            dfs(visited, A, B, i + 1 , sum + A[depth] * B[i]);
+            visited[i] = false;
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(solution(new int[]{1, 4, 2}, new int[]{5, 4, 4}) == 29);
