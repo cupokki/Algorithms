31df6acd710ace2ad893f26d0224bad1810d4b91
cupokki <nd1346@gmail.com>
2025-09-30 23:59:35 +0900

    solved : DFS + 메모이제이션, 재귀로인한 메모리 초과

--- src/main/java/programmers/n12913/Solution.java
+++ src/main/java/programmers/n12913/Solution.java
@@ -9,20 +9,52 @@ public class Solution {
     // 점수 최대값을 구하라.
     // dfs로 해결불능 n은 최대 100,000
     // DP 문제인가?
+//    static int solution(int[][] land) {
+//        int score = 0;
+//
+//        int[][] dp = new int[land.length + 1][4];
+//
+//        for (int i = 1; i <= land.length ; i++) {
+//            for (int j = 0; j < 4; j++) {
+//                for(int k = 1; k < 4; k++) {
+//                    dp[i][j] = Math.max(dp[i][j], land[i - 1][j] + dp[i - 1][(j + k) % 4]);
+//                }
+//            }
+//        }
+//        return Arrays.stream(dp[land.length]).max().getAsInt();
+//    }
+
     static int solution(int[][] land) {
-        int score = 0;
+        memo = new int[land.length + 1][4];
+        Arrays.stream(memo).forEach(row -> Arrays.fill(row, -1));
+        for (int col = 0; col < 4; col++) {
+            memo[0][col] = land[0][col];
+        }
+        int answer = 0;
+        for (int col = 0; col < 4; col++) {
+            answer = Math.max(dfs(land, land.length - 1, col), answer);
+        }
 
-        int[][] dp = new int[land.length + 1][4];
+        return answer;
+    }
+    static int[][] memo;
+    static int dfs(int[][] land, int depth, int col) {
+        if (depth == 0) return memo[0][col];
+        // 명시적으로, 사실 초기화되어있서 위가 없어도 아래 조건문에 걸림
 
-        for (int i = 1; i <= land.length ; i++) {
-            for (int j = 0; j < 4; j++) {
-                for(int k = 1; k < 4; k++) {
-                    dp[i][j] = Math.max(dp[i][j], land[i - 1][j] + dp[i - 1][(j + k) % 4]);
-                }
+        if (memo[depth][col] != -1) {
+            return memo[depth][col];
+        }
+
+        int max = 0;
+        for (int k = 0; k < 4; k++) {
+            if (k != col) {
+                max = Math.max(max, dfs(land, depth - 1, k));
             }
         }
-        return Arrays.stream(dp[land.length]).max().getAsInt();
+        return memo[depth][col] = land[depth][col] + max;
     }
+
 
     public static void main(String[] args) {
         int[][] land = {
