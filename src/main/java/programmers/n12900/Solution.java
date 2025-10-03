69ff35ae58fd80c1745ee96160495401dbc26f4d
cupokki <nd1346@gmail.com>
2025-10-04 00:07:05 +0900

    solved

--- src/main/java/programmers/n12900/Solution.java
+++ src/main/java/programmers/n12900/Solution.java
@@ -2,17 +2,29 @@ package programmers.n12900;
 
 public class Solution {
     static final int MOD = 1_000_000_007;
+//    public static int solution(int n) {
+//        int[] dp = new int[n + 1];
+//
+//        dp[1] = 1;
+//        if (n >= 2) dp[2] = 2;
+//        for (int i = 3; i <= n; i++) {
+//            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+//        }
+//
+//        return dp[n];
+//    }
     public static int solution(int n) {
-        int[] dp = new int[n + 1];
-
-        dp[1] = 1;
-        if (n >= 2) dp[2] = 2;
-        if (n >= 3) dp[3] = 3;
-        for (int i = 4; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        memo = new int[n + 1];
+        memo[1] = 1;
+        if (n >= 2) memo[2] = 2;
+        return func(n);
+    }
+    static int[] memo;
+    static int func(int i) {
+        if (memo[i] != 0) {
+            return memo[i];
         }
-
-        return dp[n];
+        return memo[i] = (func(i - 1) + func(i - 2)) % MOD;
     }
 
     public static void main(String[] args) {
