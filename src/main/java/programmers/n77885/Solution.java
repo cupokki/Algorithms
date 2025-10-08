2c248d2c65f34355403ffb4da1fa52eabe288afd
cupokki <nd1346@gmail.com>
2025-10-08 15:22:53 +0900

    solved

--- src/main/java/programmers/n77885/Solution.java
+++ src/main/java/programmers/n77885/Solution.java
@@ -9,32 +9,63 @@ public class Solution {
     // 10^15
     // 생각해보니 자릿수 차이는 커봐야 한 자리만큼 더 클 것이다.
     // 우선 1 더한 것의 2진수를 구한다.
+    /*
+    0010 -> 0011 // 짝수는 무조건 1의자리 한비트 true. 즉, + 1;
+    '01'11 -> '10'11
+      10111 ->   11011
+    1010111 -> 10110111
+    홀수라면 가장 오른쪽 "01"을 "10"으로 치환한다.
+     */
 
+    static Map<Long, Long> memo;
 
-    static Map<Long,Long> memo;
     public static long[] solution(long[] numbers) {
         memo = new HashMap<>();
         return Arrays.stream(numbers).map(Solution::func).toArray();
     }
+
     static long func(long num) {
         if (memo.containsKey(num)) {
             return memo.get(num);
         }
 
-        long temp = num + 1;
-
-        // 비트카운트가 같은수가 아닌이산 0일 순 없음
-        while(Long.bitCount(num ^ temp) > 2) {
-            temp++;
+        // 홀수
+        if (num % 2 == 0) { // == num & 1
+            return num + 1;
         }
+        // 짝수
+        long bit = (~num) & (num + 1); // 가장 오른쪽 0인 비트
+        long result = num + bit; // 가장 오른쪽 0 비트의 왼쪽을 1로
+        result -= bit >> 1; // 오른쪽 0비트 오른족의 비트만큼 빼서 "10"완성
+        memo.put(num, result);
+        return result;
+        /*
+        // 0 찾기
+        (~111) & (111 + 1)
+            = 111...1000 & 000...1000
+            = 0000...1000
 
-        memo.put(num, temp);
-        return temp;
+         result = (0111 + 1000) - (1000 >> 1)
+            = (0111 + 1000) - 0100
+            = 1111 + 0100
+            = 1011
+
+         */
+
+
+//        String bin = "0" + Long.toBinaryString(num);
+//        int idx = bin.lastIndexOf("01");
+//        bin = bin.substring(0, idx) + "10" + bin.substring(idx + 2);
+//        long result = Long.parseLong(bin, 2);
+//        memo.put(num, result);
+//        return result;
+
+
     }
 
     public static void main(String[] args) {
-//        var a = solution(new long[]{2, 7});
-        var a = solution(new long[]{7});
+        var a = solution(new long[]{2, 7});
+//        var a = solution(new long[]{7});
         System.out.println();
     }
 }
