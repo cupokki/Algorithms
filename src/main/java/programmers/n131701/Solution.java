2b1d5d5d730b2f1c89d9ff8114e2ab54098b85b9
cupokki <nd1346@gmail.com>
2025-10-20 23:40:54 +0900

    temp

--- src/main/java/programmers/n131701/Solution.java
+++ src/main/java/programmers/n131701/Solution.java
@@ -7,14 +7,22 @@ public class Solution {
     static int solution(int[] elements) {
         Set<Integer> set = new HashSet<>();
         int n = elements.length;
+        int[][] prefixSum = new int[n + 1][n + 1];
 
-        for (int idx = 0; idx < n; idx++) {
-            for(int i = 0; i < n; i++) {
-                int sum = 0;
-                for (int j = 0; j <= i; j++) {
-                    sum += elements[(idx + j) % n];
-                }
-                set.add(sum);
+        // 몇부터 몇까지 합이아니라
+        // n부터 시작했을때 n + m 까지의 누적합
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n; j++) {
+                prefixSum[i][j] = prefixSum[i][(i + j - 1) % n] + elements[(i + j) % n]; // 여기 인덱스가 잘못된듯
+
+
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                set.add(prefixSum[i][j]);
             }
         }
 
