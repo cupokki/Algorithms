f74c018acd31ace24bd7e7fffd6d9c82c6739324
cupokki <nd1346@gmail.com>
2025-11-07 23:44:55 +0900

    temp

--- src/main/java/programmers/n389479/Solution.java
+++ src/main/java/programmers/n389479/Solution.java
@@ -1,5 +1,8 @@
 package programmers.n389479;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
     /*
     m명 늘때마다 서버 한대씩 추가한다.
@@ -10,26 +13,35 @@ public class Solution {
     public static int solution(int[] players, int m, int k) {
         int answer = 0;
 
-        int server;
-        for (int i = 0; i < players.length; i++) {
-//            if(players[i] < server) {
-//                   answer++;
-//            }
-
+        Queue<Integer> pq = new LinkedList<>();
+        int scale = 0;
+        for (int t = 0; t < players.length; t++) {
+            if (players[t] >= scale * m) {
+                int n = players[t] / m - scale;
+                for (int i = 0; i < n; i++) {
+                    answer++;
+                    scale++;
+                    pq.offer(t + k);
+                }
+            }
+            while(!pq.isEmpty() && pq.peek() <= t) {
+                scale--;
+                pq.poll();
+            }
         }
         return answer;
     }
 
     public static void main(String[] args) {
-        System.out.println(
-                solution(new int[]{0, 2, 3, 3, 1, 2, 0, 0, 0, 0, 4, 2, 0, 6, 0, 4, 2, 13, 3, 5, 10, 0, 1, 5},
-                3, 4)
-        );
-
-        System.out.println(
-                solution(new int[]{0, 0, 0, 10, 0, 12, 0, 15, 0, 1, 0, 1, 0, 0, 0, 5, 0, 0, 11, 0, 8, 0, 0, 0},
-                        5, 1)
-        );
+//        System.out.println(
+//                solution(new int[]{0, 2, 3, 3, 1, 2, 0, 0, 0, 0, 4, 2, 0, 6, 0, 4, 2, 13, 3, 5, 10, 0, 1, 5},
+//                3, 5)
+//        );
+//
+//        System.out.println(
+//                solution(new int[]{0, 0, 0, 10, 0, 12, 0, 15, 0, 1, 0, 1, 0, 0, 0, 5, 0, 0, 11, 0, 8, 0, 0, 0},
+//                        5, 1)
+//        );
 
         System.out.println(
                 solution(new int[]{0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 5, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1},
