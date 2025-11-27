cd39ce1586f6f307b40a8fe5999ca6306ef8c0e6
cupokki <nd1346@gmail.com>
2025-11-27 23:51:03 +0900

    temp

--- src/main/java/programmers/n17684/Solution.java
+++ src/main/java/programmers/n17684/Solution.java
@@ -13,14 +13,15 @@ public class Solution {
             index++;
         }
         int len = msg.length();
-        msg += " ";
+//        msg += " ";
         for (int i = 0; i < len;) {
             String prev = msg.charAt(i) + "";
             int j = i + 1;
             String current = msg.substring(i, j);
-            while (map.containsKey(current)) {
+            while (j <= len && map.containsKey(current)) {
                 prev = current;
                 j++;
+                if (j > len) break; // 끝에 도달했으면 substring 안 하고 빠져나감
                 current = msg.substring(i, j);
             }
             result.add(map.get(prev));
