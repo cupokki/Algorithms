86c480a1f055caa52d00ce8427fa0b2ece2679e3
cupokki <nd1346@gmail.com>
2025-11-28 22:02:49 +0900

    temp

--- src/main/java/programmers/n17683/Solution.java
+++ src/main/java/programmers/n17683/Solution.java
@@ -14,6 +14,8 @@ class Solution {
     - 없다면 "(None)" 출력
 
     곡명이 긴 순으로 정렬하고, 재생시간이 빠른 순으로 정렬한다.
+
+    **C와 C# 같은 경우를 구분하지 못한다.
     */
     public static String solution(String m, String[] musicinfos) {
         String answer = "(None)";
@@ -25,32 +27,44 @@ class Solution {
         });
         for(String s : musicinfos) {
             String[] token = s.split(",");
-            int min = getMin(token[0], token[1]);
-            String runningMin = String.valueOf(min);
+            int duration = getMin(token[0], token[1]);
+            String runningMin = String.valueOf(duration);
             String title = token[2];
-            String convertedMelody = "";
-            char[] temp = token[3].toCharArray();
-            for (int i = 0; i < min; i++) {
-                if (i + 1 < temp.length && temp[i + 1] == '#') {
-                    convertedMelody += temp[i % temp.length] + temp[(i + 1) % temp.length];
-                    i++;
-                } else {
-                    convertedMelody += temp[i % temp.length];
-                }
-            }
+            String convertedMelody = convertString(token[3], duration);
+
 
             pq.offer(new String[]{runningMin, title, convertedMelody});
         }
 
+        int sharp = (int) m.chars().filter(c-> c == '#').count();
+        String target = convertString(m, m.length() - sharp);
+
         while (!pq.isEmpty()) {
             String[] str = pq.poll();
-            if (str[2].contains(m)){
+            if (str[2].contains(target)){
                 answer = str[1];
                 break;
             }
         }
 
         return answer;
+    }
+    static String convertString(String str, int n) {
+        String result = "";
+        char[] temp = str.toCharArray();
+        int sIdx = 0;
+        int idx = 0;
+        while (idx != n) {
+            if (temp[(sIdx + 1) % temp.length] == '#') {
+                result += temp[sIdx % temp.length] + "#";
+                sIdx++;
+            } else {
+                result += temp[sIdx % temp.length] + " ";
+            }
+            sIdx++;
+            idx++;
+        }
+        return result;
     }
     static int getMin(String s, String e) {
         String[] start = s.split(":");
@@ -64,7 +78,7 @@ class Solution {
 
     public static void main(String[] args) {
 //        System.out.println(solution("ABCDEFG", new String[]{"12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"}));
-//        System.out.println(solution("CC#BCC#BCC#BCC#B", new String[]{"03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"}));
-        System.out.println(solution("ABC", new String[]{"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"}));
+        System.out.println(solution("CC#BCC#BCC#BCC#B", new String[]{"03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"}));
+//        System.out.println(solution("ABC", new String[]{"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"}));
     }
 }
