8d3819084d9b23f9491af5a282d42c7f6506f49a
cupokki <nd1346@gmail.com>
2025-12-03 17:56:07 +0900

    solved

--- src/main/java/programmers/n12936/Solution.java
+++ src/main/java/programmers/n12936/Solution.java
@@ -1,6 +1,9 @@
 package programmers.n12936;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class Solution {
     /*
@@ -52,33 +55,35 @@ public class Solution {
 //    }
     static int[] solution(int n, long k) {
         int len = n;
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= n; i++)
+            numbers.add(i);
         int[] answer = new int[len];
 
+        long[] fac = new long[len + 1];
+        fac[0] = 1;
+        for (int i = 1; i <= n; i++){
+            fac[i] = fac[i - 1] * i;
+        }
         int idx = 0;
+        k--;
         while(idx < len) {
-            int fac = fac(n - 1);
-            answer[idx] = (int) ((k - 1) / fac);
-            System.out.print(answer[idx] + " ");
-            k = (k - 1) % fac + 1;
-            n--;
+            int pos = (int) (k / fac[n - 1 - idx]);
+            answer[idx] = numbers.remove(pos);
+//            System.out.print(answer[idx] + " ");
+            k %= fac[n - 1 - idx];
             idx++;
         }
-        System.out.println();
-
-        Arrays.stream(answer).forEach(i -> System.out.print(i + " "));
-        System.out.println();
+//        System.out.println();
+//
+//        Arrays.stream(answer).forEach(i -> System.out.print(i + " "));
+//        System.out.println();
 
         return answer;
-    }
-    static int fac(int n) {
-        if (n == 1) {
-            return 1;
-        }
-        return n * fac(n - 1);
     }
 
     public static void main(String[] args) {
         solution(3, 5);
-//        solution(3, 2);
+        solution(3, 2);
     }
 }
