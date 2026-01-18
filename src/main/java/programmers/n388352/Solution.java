bab79cc21644b07cb8fdc9aee022607d01fca5d7
cupokki <nd1346@gmail.com>
2026-01-18 14:22:29 +0900

    solved

--- src/main/java/programmers/n388352/Solution.java
+++ src/main/java/programmers/n388352/Solution.java
@@ -1,7 +1,5 @@
 package programmers.n388352;
 
-import java.util.Arrays;
-
 public class Solution {
     /*
     정보 해독한다.
@@ -11,54 +9,77 @@ public class Solution {
      - m번 수행 이후 가능한 조합의 수를 알고 싶다.
 
      n은 30이하
+     ans 는 시스템의 응답
+     m은 질문 횟 수, 10이하의 자연수
+
+     C(10, 2) = 45 * C(5, 2);
+     // 각 query 를 비교한다. 어떻게?
+
+     C(30, 5)는 약 14만? 가능한가 -> 길이가 5로 제한되니까 dfs 불가능하진 않겠네
+     n이하의 자연수 5개를 선택하는 dfs돌려서 q와 ans 조건에 만족하는 것의 개수를 직접 세는 방식?
      */
-    static boolean[] candidates;
-    static boolean[] answer;
-    static int N;
-    public static int solution(int n, int[][]q, int[] ans) {
+    static int[][] query;
+    static int[] answer;
+    static int N, M;
+    static int result;
+
+    public static int solution(int n, int[][] q, int[] ans) {
         N = n;
-        candidates = new boolean[N + 1];
-        answer = new boolean[N + 1];
-        for (int i = 0; i < ans.length; i++) {
-            answer[ans[i]] = true;
-        }
-        for (int[] query: q) {
-            int result = func(query);
-        }
-
-        int num = 0;
-        for (boolean b : candidates) {
-            num += b ? 1 : 0;
-        }
-
-        return 0;
-    }
-
-    static int func(int[] query) {
-//        boolean[] candidates = new boolean[N + 1];
-        for (int i = 0; i < query.length; i++) {
-            if (answer[query[i]])
-                candidates[query[i]] = true;
-        }
-
-        int result = 0;
-        for (int i = 0; i < candidates.length; i++) {
-            if(candidates[i]) {
-                result++;
-            }
-        }
+        M = q.length;
+        query = q;
+        answer = ans;
+        result = 0;
+        selected = new int[5];
+        dfs(0, 1);
 
         return result;
     }
 
+    static int[] selected;
+
+    static void dfs(int depth, int start) {
+        if (depth == 5) {
+            // q의 모든 수와 비교
+            for (int i = 0; i < M; i++) {
+                int cnt = 0;
+                for (int j = 0; j < 5; j++) {
+                    for (int k = 0; k < 5; k++) {
+                        if (selected[j] == query[i][k]) {
+                            cnt++;
+                        }
+                    }
+                }
+                if (cnt != answer[i]) {
+                    return;
+                }
+            }
+            result++;
+            for (int i = 0; i < 5; i++) {
+                System.out.print(String.format("%3d", selected[i]));
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            selected[depth] = i;
+            dfs(depth + 1, i + 1);
+        }
+    }
+
+
     public static void main(String[] args) {
-        var q1 =  new int[][] {
+        var q1 = new int[][]{
                 {1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {3, 7, 8, 9, 10}, {2, 5, 7, 9, 10}, {3, 4, 5, 6, 7}
         };
         System.out.println(solution(10, q1, new int[]{2, 3, 4, 3, 3}));
-        var q2 =  new int[][] {
+        var q2 = new int[][]{
                 {2, 3, 9, 12, 13}, {1, 4, 6, 7, 9}, {1, 2, 8, 10, 12}, {6, 7, 11, 13, 15}, {1, 4, 10, 11, 14}
         };
         System.out.println(solution(15, q2, new int[]{2, 1, 3, 0, 1}));
+        var q3 = new int[][]{
+                {1, 2, 3, 4, 5}
+        };
+        System.out.println(solution(10, q3, new int[]{1}));
     }
 }
