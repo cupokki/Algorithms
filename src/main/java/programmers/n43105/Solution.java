3b6bd9b71e42c1c4042a69fb660a0a56faacd200
cupokki <nd1346@gmail.com>
2026-02-09 23:41:10 +0900

    solved

--- src/main/java/programmers/n43105/Solution.java
+++ src/main/java/programmers/n43105/Solution.java
@@ -1,6 +1,6 @@
 package programmers.n43105;
 
-import java.util.Arrays;
+import javax.management.monitor.StringMonitor;
 
 public class Solution {
     /*
@@ -8,28 +8,43 @@ public class Solution {
     꼭대기에서 바닥까지 수를 이어가며 바닥으로 향한다.
     바닥으로 향하는 경로의 수를 다 더한 합이 가장 큰 경우를 구하라.
      */
+//    public static int solution(int[][] triangle) {
+//        int n = triangle.length;
+//        int[][] dp = new int[n][n];
+//
+//        dp[0][0] = triangle[0][0];
+//        for (int i = 1; i < n; i++) {
+//            dp[i][0] = dp[i - 1][0] + triangle[i][0];
+//            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
+//            for (int j = 1; j < i; j++) {
+//                dp[i][j] = Math.max(
+//                        dp[i - 1][j - 1] + triangle[i][j],
+//                        dp[i - 1][j] + triangle[i][j]
+//                );
+//            }
+//        }
+//        int answer = Arrays.stream(dp[n - 1]).max().getAsInt();
+//        return answer;
+//    }
+
     public static int solution(int[][] triangle) {
         int n = triangle.length;
-        int[][] dp = new int[n][n];
 
-        dp[0][0] = triangle[0][0];
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = dp[i - 1][0] + triangle[i][0];
-            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
-            for (int j = 1; j < i; j++) {
-                dp[i][j] = Math.max(
-                        dp[i - 1][j - 1] + triangle[i][j],
-                        dp[i - 1][j] + triangle[i][j]
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] += Math.max(
+                        triangle[i + 1][j],
+                        triangle[i + 1][j + 1]
                 );
             }
         }
-        int answer = Arrays.stream(dp[n - 1]).max().getAsInt();
-        return answer;
+
+        return triangle[0][0];
     }
 
     public static void main(String[] args) {
         System.out.println(solution(
-                new int[][] {
+                new int[][]{
                         {7},
                         {3, 8},
                         {8, 1, 0},
