88ea6afcbd9f06ddb73bc33648fcf1e9c4f614d7
cupokki <nd1346@gmail.com>
2026-03-06 12:03:00 +0900

    solved

--- src/main/java/programmers/n43164/Solution.java
+++ src/main/java/programmers/n43164/Solution.java
@@ -12,11 +12,18 @@ public class Solution {
     경로가 2개 이상일 경우 알파벳 순으로 앞서는 경로
 
     사이클이 있나? 예제 2를 보면 사이클이 있다.
-     */
-    public static String[] solution(String[][] tickets) {
-        String[] answer = new String[tickets.length + 1];
 
-        Map<String, PriorityQueue<String>> graph = new HashMap<>();
+    지금 bfs는 안된다.
+    우선 순위가 무조건 사전순부터라 경로를 만들 수 있음에도 조기에 종료된다.
+
+    백트래킹으로 단 하나의 경로를 찾는다?
+     */
+    static List<String> list;
+    static Map<String, PriorityQueue<String>> graph;
+    public static String[] solution(String[][] tickets) {
+        String[] answer;
+        list = new ArrayList<>();
+        graph = new HashMap<>();
         for (String[] ticket : tickets) {
             graph.computeIfAbsent(
                     ticket[0],
@@ -27,28 +34,32 @@ public class Solution {
                     k -> new PriorityQueue<>()
             );
         }
-
-        Queue<String> q = new LinkedList<>();
-        Set<String> usedEdge = new HashSet<>();
-        q.offer("ICN");
-        answer[0] = "ICN";
-        int idx = 1;
-        while (!q.isEmpty()) {
-            String cur = q.poll();
-
-            if(graph.get(cur).size() == 0) break;
-            String next = graph.get(cur).poll();
-            q.offer(next);
-            answer[idx++] = next;
-        }
+        dfs("ICN");
+        Collections.reverse(list);
+        answer = list.toArray(String[]::new);
         return answer;
     }
 
+    static void dfs(String cur) {
+        var pq = graph.get(cur);
+//        if (pq.isEmpty()) {
+//            list.add(cur);
+//            return;
+//        }
+
+        while (!pq.isEmpty()) {
+            String next = pq.poll();
+            dfs(next);
+        }
+
+        list.add(cur);
+    }
+
     public static void main(String[] args) {
-//        Arrays.stream(solution(new String[][]{
-//                {"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}
-//        })).forEach((i) -> System.out.print(i + " "));
-//        System.out.println();
+        Arrays.stream(solution(new String[][]{
+                {"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}
+        })).forEach((i) -> System.out.print(i + " "));
+        System.out.println();
 
         Arrays.stream(solution(new String[][]{
                 {"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}
