9cb6d82e55952943d0fa9e742d1597a45f9714ea
cupokki <nd1346@gmail.com>
2026-03-09 23:46:10 +0900

    temp

---
+++ src/main/java/programmers/n43238/Solution.java
@@ -0,0 +1,55 @@
+package programmers.n43238;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Solution {
+    /*
+    n명의 입국심사 대기줄
+    심사관마다 소모시간 다름, 심사대당 동시에 한명만 가능
+    모두 빈 심사대로 시작하여, 줄의 앞사람은 빈 심사대로.
+    하지만 더 빨리 끝나는 심사대가 있다면, 기다렸다가 그곳으로 가서 심사 받을 수 있다.
+
+    모두 심사받는 최소의 시간을 구하라.
+
+    심사관은 10만명 이하의 자연수
+    소요시간은 10억분 이하
+    n은 10억 아하의 자연수
+
+    10만 케이스를 비교한다?;
+
+    먼저 끝나는 시간을 비교하는게 아니라 끝나는 시간을 비교한다.
+
+
+     */
+    public static long solution(int n, int[] times) {
+        long answer = 0;
+
+        int l = 1, r = 1_000_000_000;
+        int m = (l + r) / 2;
+        while (l <= r) {
+            m = (l + r) / 2;
+            if (check(times, m) >= n) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+
+        }
+
+        answer = m;
+        return answer;
+    }
+    static int check(int[] times, int t) {
+        int sum = 0;
+        for (int i = 0; i < times.length; i++) {
+            sum += t / times[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(6, new int[]{7, 10})); // 28
+    }
+}
