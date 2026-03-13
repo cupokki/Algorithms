79f5e4dbdface1b5fb79cf142f45a59aa36ef1c4
cupokki <nd1346@gmail.com>
2026-03-13 23:58:34 +0900

    temp

---
+++ src/main/java/programmers/n77486/Solution.java
@@ -0,0 +1,76 @@
+package programmers.n77486;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    /*
+    다단계 조직도
+    판매원이 판매하면 이익이 트리를 타고 분배된다.
+    민호는 center로써 누가 얼마나 이익을 가져갔는지 알아본다.
+
+    판매원은 판매액의 10%를 추천인에게 배분하고 나머지는 본인이 가진다.
+    또한 피추천자의 판매액 배분 또한 본인의 이익으로 산정되어 10%를 추천인에게 분배하고, 나머지를 본인이 가진다.
+
+    원단위 절사
+
+
+    말단노드부터 해야하는거 아닌가? = 자식이 없는 노드
+    말단 노드라도 깊이가 깊은거 부터 해야하는거 아닌가?
+    깊은 노드부터 안하면 다시 계산해야함..
+     */
+    public static int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int n = enroll.length;
+        Map<String, String> tree = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String parent = referral[i];
+            if (parent.equals("-")) parent = "center";
+            tree.put(enroll[i], parent);
+        }
+
+
+        int m = seller.length;
+        Map<String, Integer> incomes = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            incomes.put(enroll[i], 0);
+        }
+        for (int i = 0; i < m; i++) {
+            String current = seller[i];
+            String parent = tree.get(seller[i]);
+            int remain = 100 * amount[i];
+            while(!current.equals("center")) {
+
+                incomes.merge(current, (int)Math.floor(remain * 0.9), Integer::sum);
+                remain = (int)Math.floor(remain * 0.1);
+
+                current = parent;
+                parent = tree.get(current);
+            }
+        }
+        int[] answer = new int[n]; // 셀러당 판매량
+        for (int i = 0; i < n; i++) {
+            answer[i] = incomes.get(enroll[i]);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Arrays.stream(solution(
+                new String[] {"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"},
+                new String[] {"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"},
+                new String[] {"young", "john", "tod", "emily", "mary"},
+                new int[] {12, 4, 2, 5, 10}
+        )).forEach(i -> System.out.print(i + " "));
+        System.out.println();
+
+        Arrays.stream(solution(
+                new String[] {"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"},
+                new String[] {"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"},
+                new String[] {"sam", "emily", "jaimie", "edward"},
+                new int[] {2, 3, 5, 4}
+        )).forEach(i -> System.out.print(i + " "));
+        System.out.println();
+    }
+}
