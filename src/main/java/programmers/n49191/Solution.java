bb56a465b1188753e65a9ced112ca93eeeabf8dc
cupokki <nd1346@gmail.com>
2026-03-27 23:59:27 +0900

    temp

--- src/main/java/programmers/n49191/Solution.java
+++ src/main/java/programmers/n49191/Solution.java
@@ -13,6 +13,9 @@ public class Solution {
 
     일부 경기기록이 분실된 results가 주어질때, 순위가 확실한 사람의 수를 출력
     방향 그래프로 연결하고, 말단 노드부터 검사?
+
+
+
     */
     public static int solution(int n, int[][] results) {
         int answer = 0;
@@ -21,6 +24,28 @@ public class Solution {
 
         for (int[] edge : results) {
             graph[edge[0]][edge[1]] = true;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int temp = 0;
+            for (int j = 1; j <= n; j++) {
+                if (graph[i][j] || graph[j][i]) {
+                    temp++;
+                }
+            }
+            if (temp == n - 1) {
+                // 이 점은 순위가 확정임
+                answer++;
+
+                // i 와 연결된 모든 j에 대해
+                for (int j = 1; j <= n; j++) {
+                    if (graph[i][j]) {
+                        for (int k = 1; k <= n; k++) { //상위노드
+                            if (graph[i][j]) graph[k][j] = true;
+                        }
+                    }
+                }
+            }
         }
 
 
