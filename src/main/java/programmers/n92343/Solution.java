ecdca43003cb703962d53c7fd86512949d890b92
cupokki <nd1346@gmail.com>
2026-04-17 17:02:51 +0900

    solved

--- src/main/java/programmers/n92343/Solution.java
+++ src/main/java/programmers/n92343/Solution.java
@@ -18,48 +18,18 @@ public class Solution {
     단순 dfs
     
     */
-    static class Node {
-        public Node(int parent) {
-            this.parent = parent;
-            this.children = new HashSet<>();
-        }
 
-        int parent = -1;
-        Set<Integer> children;
-
-    }
-
-    static Map<Integer,Node> tree;
     static boolean[][] graph;
     static int n;
     static int[] Info;
     public static int solution(int[] info, int[][] edges) {
-        int answer = 0;
         n = info.length;
         Info = info;
-        boolean[] visited = new boolean[n];
-//        tree = new HashMap();
-//        for (int[] e : edges) {
-//            var p = tree.computeIfAbsent(e[0], k -> new Node(-1));
-//            p.children.add(e[1]);
-//            var c = tree.computeIfAbsent(e[1], k -> new Node(e[1]));
-//            c.parent = (e[0]);
-//        }
 
+        graph = new boolean[n][n];
         for (int[] e : edges) {
-            graph[e[0]][e[1]] = graph[e[1]][e[0]] = true;
+            graph[e[0]][e[1]] = true;
         }
-//
-//        for (int i = 1; i < n; i++) { // 루트도 간선이 하나일수도 있으므로 1부터 시작
-//            var node = tree.get(i);
-//            int cur = i;
-//            while (node.children.size() == 0 && info[cur] == 1) {
-//                visited[cur] = true;
-//                tree.get(node.parent).children.remove(cur);
-//                cur = node.parent;
-//                node = tree.get(cur);
-//            }
-//        }
 
         // bfs 하려니 양쪽을 둘러보면 문제 규칙과 맞지 않는다.
         // dfs이다.
@@ -67,34 +37,30 @@ public class Solution {
         // 프루닝은? 프루닝 대상인걸 어떻게 알지?
         // 현 가지를 더 진행할지말지는 다른 가지를 들렸다 와야 알 수 있는것 아닌가?
         // 그러면 현 가지를 보류하는 알고리즘이 있어야하는거 아닌가?
-        max = 0;
-        bfs(0, 1, 0, 1 << 0 | 1 << 1);
-        return answer;
+        max = 1;
+        int mask = 0;
+        for (int child = 0; child < n; child++) {
+            if (graph[0][child]) mask |= 1 << child;
+        }
+        bfs(0, 1, 0, mask);
+        return max;
     }
 
     static int max;
-    static void bfs(int curNode, int sheep, int wolves, int next) {
-        // 0은 루트
-//        if (wolves >= sheep) return;
-//        max = Math.max(max, sheep);
-//        for (int nextNode = 1; nextNode < n; nextNode++) {
-//            if (graph[curNode][nextNode]) {
-//                bfs(nextNode, sheep + (Info[nextNode] == 1?1:0), wolves + (Info[nextNode] == 0?1:0), next);
-//            }
-//        }
+    static void bfs(int curNode, int sheep, int wolves, int mask) {
+        if (sheep <= wolves) return;
+        if (mask == 0) return;
+//        System.out.println(String.format("From %d, s: %d, w: %d %017d", curNode, sheep, wolves, Long.parseLong(Integer.toBinaryString(mask))));
 
-        for (int i = 0; i < n; i++) {
-            if ((next & (1 << i)) == 0) continue;
-            int nextNode = i;
-            int nextSheep = sheep + (Info[nextNode] == 0?1:0);
-            int nextWolves = wolves + (Info[nextNode] == 1?1:0);
-
-            if (wolves < sheep) {
-                max = Math.max(max, nextSheep);
-                int nextMask = next & ~(1 << curNode);
-                for (int to = 0; to < n; to++) {
-                    if (graph[curNode][to]) nextMask |= 1 << to;
+        for (int nextNode = 0; nextNode < n; nextNode++) {
+            if ((mask & (1 << nextNode)) != 0) {
+                int nextSheep = sheep + (Info[nextNode] == 0 ? 1 : 0);
+                int nextWolves = wolves + (Info[nextNode] == 1 ? 1 : 0);
+                int nextMask = mask & ~(1 << nextNode); // 현노드 방문 처리
+                for (int child = 0; child < n; child++) {
+                    if (graph[nextNode][child] && nextSheep > nextWolves) nextMask |= 1 << child;
                 }
+                max = Math.max(max, nextSheep);
                 bfs(nextNode, nextSheep, nextWolves, nextMask);
             }
         }
@@ -106,7 +72,7 @@ public class Solution {
         }));
 
         System.out.println(solution(new int[]{0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0}, new int[][]{
-                {0, 1}, {1, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 6}, {3, 7}, {4, 8}, {6, 9}, {9, 10}
+                {0, 1}, {0, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 6}, {3, 7}, {4, 8}, {6, 9}, {9, 10}
         }));
     }
 }
