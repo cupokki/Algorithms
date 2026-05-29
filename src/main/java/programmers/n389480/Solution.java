6e5a94a00949d6203823a61fc7360fe08e3655fb
cupokki <nd1346@gmail.com>
2026-05-29 23:51:47 +0900

    temp

--- src/main/java/programmers/n389480/Solution.java
+++ src/main/java/programmers/n389480/Solution.java
@@ -14,34 +14,31 @@ public class Solution {
 
     dfs는 불가?
     */
-    static int[] result;
+    static int[][] memo;
     static int min;
     static int M, N;
     public static int solution(int[][] info, int n, int m) {
-//        result = new int[info.length];
-//        min = Integer.MAX_VALUE;
-//        N = n; M = m;
-//        dfs(info, 0, 0, 0);
-//        return min == Integer.MAX_VALUE ? -1 : min;
-
-        int[][] dp = new int[info.length][2];
-
-        for (int i = 0; i < info.length; i++) {
-            // i까지 고려했을때 최소로 물건을 선택하는
-        }
-
-        return -1;
+        memo = new int[info.length][121];
+        N = n; M = m;
+        dfs(info, 0, 0);
+        return min == Integer.MAX_VALUE ? -1 : min;
     }
 
-//    static void dfs(int[][] info, int a, int b, int depth) {
-//        if (depth == info.length) {
-//            min = Math.min(min, a);
-//            return;
-//        }
-//
-//        if (a + info[depth][0] < N) dfs(info, a + info[depth][0], b, depth + 1);
-//        if (b + info[depth][1] < M) dfs(info, a, b + info[depth][1], depth + 1);
-//    }
+    static int dfs(int[][] info, int b, int depth) {
+        if (depth == info.length) {
+//            min = Math.min(min, );
+            return min;
+        }
+
+        if (memo[depth][b] != 0) {
+            return memo[depth][b];
+        }
+        memo[depth][b] = dfs(info, b, depth + 1);
+        if (memo[depth][b] + info[depth][1] < N) return memo[depth][b] + info[depth][0];
+//        if (b + info[depth][1] < M)
+            return dfs(info, b + info[depth][1], depth + 1);
+
+    }
 
     public static void main(String[] args) {
         System.out.println(solution(new int[][]{{1, 2}, {2, 3}, {2, 1}}, 4, 4));
