0c630531e3a9586c8d4fe6083f5bdd33b25feedf
cupokki <nd1346@gmail.com>
2026-06-01 21:30:48 +0900

    solved

--- src/main/java/programmers/n60062/Solution.java
+++ src/main/java/programmers/n60062/Solution.java
@@ -1,54 +1,74 @@
 package programmers.n60062;
 
-public class Solution {
+import java.util.*;
+
+class Solution {
     /*
-    원형의 공간 둘레 n
-    외벽에 취약점 존재
-    1시간마다 이동거리가 제각각인 인부를 보내 벽을 점검
-    정북방향부터 시계방향으로 거리를 나타냄
-    인부는 시계 혹은 반시계로 외벽을 따라 이동
-    취약지점을 점검하기 위해 보내야하는 친구
-    의 최소 수를 구하라.
-    모두 투입해도 불가능 하다면 -1
+    원형 외벽을 점검한다. 둘레는 n
+    weak: 취약지점이 주어진다.
+    dist: 시계방향 또는 반시계로 dist[i]만큼 이동하는 일꾼
+    모든 취약지점을 고려하는 필요한 최소의 친구 수를 출력한다.
+    실패시 -1;
 
-    인부가 출발할지점을 고를 수 있다.
 
-    n은 2이상 200이하 자연수
-
-     */
+    */
+    static int min;
     public static int solution(int n, int[] weak, int[] dist) {
-        result = new int[dist.length];
-        dfs(n, weak, dist, 0, 0);
-        return min == Integer.MAX_VALUE ? -1 : min;
+        int answer = -1;
+        min = 9999999;
+
+        int[] convertedWeak = new int[weak.length * 2];
+        for (int i = 0; i < weak.length; i++) {
+            convertedWeak[i] = weak[i];
+            convertedWeak[i + weak.length] = weak[i] + n;
+        }
+
+        int[] descDist = Arrays.stream(dist)
+                .boxed()
+                .sorted(Collections.reverseOrder())
+                .mapToInt(i -> i)
+                .toArray();
+
+        backtrack(new boolean[weak.length], n, convertedWeak, descDist, 0, 0);
+        return min  == 9999999 ? -1 : min;
     }
 
-    static int[] result;
-    static int min;
-    static void dfs(int n, int[] weak, int[] dist, int depth, int start) {
+    static void backtrack(boolean[] visited, int n, int[] weak, int[] dist, int depth, int sum) {
         if (depth == dist.length) {
-            int wIdx = 0;
-            for (int i = 0; i < dist.length; i++) {
-                while (weak[wIdx] < wIdx + result[i]) { // 범위가 인덱스가 잘못됨
-                    wIdx++;
-                }
-                wIdx++;
-                if (wIdx == weak.length) {
-                    min = Math.min(min, i);
-                    break;
-                }
+            for (int i = 0 ; i < weak.length / 2; i++) {
+                if (!visited[i]) return;
             }
-
+            min = Math.min(sum, min);
             return;
         }
 
-        for (int i = start ; i < dist.length; i++) {
-            result[depth] = dist[i];
-            dfs(n, weak, dist, depth + 1, depth + 1);
+        if (min <= sum) {
+            return;
+        }
+
+        backtrack(visited, n, weak, dist, depth + 1, sum); // skip
+
+        for (int i = 0; i < weak.length / 2; i++)  {
+            if (visited[i]) continue;
+
+            int j = 0;
+            List<Integer> list = new ArrayList<>();
+            while(i + j < weak.length && weak[i + j] - weak[i] <= dist[depth]){
+                if (visited[(i + j) % (weak.length / 2)]) { // 이미 방문한 친구 롤백방지
+                    j++;
+                    continue;
+                }
+                visited[(i + j) % (weak.length / 2)] = true;
+                list.add(i + j);
+                j++;
+            }
+            backtrack(visited, n, weak, dist, depth + 1, sum + 1);
+            for (int idx : list)
+                visited[idx % (weak.length / 2)] = false;
         }
     }
 
     public static void main(String[] args) {
-
         System.out.println(solution(12, new int[]{1, 5, 6, 10}, new int[]{1, 2, 3, 4}));
         System.out.println(solution(12, new int[]{1, 3, 4, 9, 10}, new int[]{3, 5, 7}));
 
