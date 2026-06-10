0741c836aa64ff849b9e26a2e13179890165e633
cupokki <nd1346@gmail.com>
2026-06-10 23:45:33 +0900

    temp

--- src/main/java/programmers/n42895/Solution.java
+++ src/main/java/programmers/n42895/Solution.java
@@ -13,54 +13,54 @@ public class Solution {
     number: 32000이하, 만들 수
     N: 9이하 자연수, 사용할 숫자
 
+    계산중에 음수가 되는게 올바른가?
+    계산중에 값이 어디까지 허용되야하는가
+    => 관점은 이런 계산과정이 최소가 되게 하는것인지가 중요한데
+    5로 111을 만들때, 가장 빠른게 555/5 이다.
+
     */
     public static int solution(int N, int number) {
-        memo = new int[number + 1];
+        memo = new int[32000 + 1];
         Arrays.fill(memo, Integer.MAX_VALUE / 2);
-        int answer = backtrack(N, 0, number);
-        return answer > 32000 ? -1 : answer;
+        int answer = INF;
+        int term = N;
+        int i = 1;
+        while (term < memo.length) {
+            answer = Math.min(answer, backtrack(N, i, number));
+            term = term * 10 + N;
+        }
+        return answer;
+//        return answer > 32000 ? -1 : answer;
     }
 
     static int[] memo;
-
+    static final int INF = Integer.MAX_VALUE / 2;
     // 반환값: num을 만드는 최소 수
-    static int backtrack(int N, int depth, int num) {
-        if (depth == 9) {
-            return Integer.MAX_VALUE / 2; // 답이 될 수 없다.
+    static int backtrack(int N, int depth, int num ) {
+        if (num == 0) {
+            return 0;
         }
 
-        if (memo[num] != Integer.MAX_VALUE / 2) {
+        if (depth > 8) {
+            return INF;
+        }
+
+        if (memo[num] != INF) {
             return memo[num];
         }
 
-        int min = Integer.MAX_VALUE;
+        int min = INF;
 
-        // num * 10 + N;
-        // 이건 특별한 경우만 가능하다 수가 이어진 경우만..
-        if (num * 10 + N <= 32000)
-            min = Math.min(min, backtrack(N, depth + 1, num * 10 + N) + 1);
-
-        // 5 더하기
-        if (num + 5 <= 32000)
-            min = Math.min(min, backtrack(N, depth + 1, num + 5) + 1);
-
-        // 5 빼기
-        if (num - 5 > 0)
-            min = Math.min(min, backtrack(N, depth + 1, num - 5) + 1);
-
-        // 5 곱하기
-        if (num * 5 <= 32000)
-            min = Math.min(min, backtrack(N, depth + 1, num * 5) + 1);
-
-        // 5 나누기
-        if (num / 5 > 0)
-            min = Math.min(min, backtrack(N, depth + 1, num / 5) + 1);
+        if (num - N > 0) min = Math.min(min, backtrack(N, depth + 1, num - N) + 1); // N 더하기
+        if (num + N <= memo.length) min = Math.min(min, backtrack(N, depth + 1, num + N) + 1); // N 빼기 -> 더해야할 것이 N 만큼 는다.
+        if (num + N <= memo.length) min = Math.min(min, backtrack(N, depth + 1, num * N) + 1); // 남은 수를 N으로 곱하기
+        if (num - N > 0) min = Math.min(min, backtrack(N, depth + 1, num / N) + 1); // 남은 수를 N으로 나누기
 
         return memo[num] = min;
     }
 
      public static void main (String[] args) {
          System.out.println(solution(5, 12)); // 4
-         System.out.println(solution(2, 11)); //3
+         System.out.println(solution(2, 11)); // 3
      }
 }
