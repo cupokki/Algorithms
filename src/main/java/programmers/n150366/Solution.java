11446142c8f016b3676f521c5a21d6c72b0324d1
cupokki <nd1346@gmail.com>
2026-06-18 23:54:21 +0900

    solved

--- src/main/java/programmers/n150366/Solution.java
+++ src/main/java/programmers/n150366/Solution.java
@@ -18,6 +18,7 @@ public class Solution {
     */
     int[][][] parents;
     String[][] values;
+
     public String[] solution(String[] commands) {
         List<String> result = new ArrayList<>();
         parents = new int[51][51][2]; // rootR, rootC
@@ -30,14 +31,15 @@ public class Solution {
                 values[i][j] = "";
             }
         }
-        for (String comm :commands) {
+        for (String comm : commands) {
             String[] tokens = comm.split(" ");
             switch (tokens[0]) {
                 case "UPDATE" -> update(tokens);
                 case "MERGE" -> merge(tokens);
                 case "UNMERGE" -> unmerge(tokens);
                 case "PRINT" -> print(result, tokens);
-                default -> {}
+                default -> {
+                }
             }
         }
 
@@ -50,43 +52,49 @@ public class Solution {
         int c1 = Integer.parseInt(tokens[2]);
         int r2 = Integer.parseInt(tokens[3]);
         int c2 = Integer.parseInt(tokens[4]);
-        String value = values[r1][c1]; // 첫번쨰 값
-        values[r1][c1] = values[r2][c2] = "";
-        int tr = r1; int tc = c1;
-        // 두번쨰 위치에 값이 존재한다면
-        if (value.equals("") && values[r2][c2] != "") {
-            tr = r2; tc = c2;
-            value = values[r2][c2];
-        }
 
+        int rootR1 = parents[r1][c1][0];
+        int rootC1 = parents[r1][c1][1];
+        int rootR2 = parents[r2][c2][0];
+        int rootC2 = parents[r2][c2][1];
+
+        if (rootR1 == rootR2 && rootC1 == rootC2) return;
+
+        int targetR = rootR1;
+        int targetC = rootC1;
+        String finalValue = values[rootR1][rootC1];
+        if (finalValue.equals("") && !values[rootR2][rootC2].equals("")) {
+            targetR = rootR2;
+            targetC = rootC2;
+            finalValue = values[rootR2][rootC2];
+        }
         for (int i = 1; i <= 50; i++) {
             for (int j = 1; j <= 50; j++) {
-                if (
-                        parents[i][j][0] == tr &&
-                        parents[i][j][1] == tc // 같은 부모를 보는 것.
+                if ((parents[i][j][0] == rootR2 && parents[i][j][1] == rootC2) ||
+                        (parents[i][j][0] == rootR1 && parents[i][j][1] == rootC1)
                 ) {
-
+                    values[i][j] = "";
+                    parents[i][j][0] = targetR;
+                    parents[i][j][1] = targetC;
                 }
             }
         }
-        parents[r1][c1][0] = parents[r2][c2][0] = tr;
-        parents[r1][c1][1] = parents[r2][c2][1] = tc;
-        values[tr][tc] = value;
+
+        values[targetR][targetC] = finalValue;
     }
 
     public void unmerge(String[] tokens) {
         int r = Integer.parseInt(tokens[1]);
         int c = Integer.parseInt(tokens[2]);
 
-        String value = values[parents[r][c][0]][parents[r][c][1]];
+        int rootR = parents[r][c][0];
+        int rootC = parents[r][c][1];
 
+        String finalValue = values[rootR][rootC];
 
         for (int i = 1; i <= 50; i++) {
             for (int j = 1; j <= 50; j++) {
-                if (
-                        parents[i][j][0] == parents[r][c][0] &&
-                        parents[i][j][1] == parents[r][c][1] // 같은 부모를 보는 것.
-                ) {
+                if (parents[i][j][0] == rootR && parents[i][j][1] == rootC) {
                     values[i][j] = "";
                     parents[i][j][0] = i;
                     parents[i][j][1] = j;
@@ -94,7 +102,7 @@ public class Solution {
             }
         }
 
-        values[r][c] = value;
+        values[r][c] = finalValue;
     }
 
     public void update(String[] tokens) {
@@ -102,19 +110,12 @@ public class Solution {
             int r = Integer.parseInt(tokens[1]);
             int c = Integer.parseInt(tokens[2]);
 
-            if (parents[r][c][0] == r && parents[r][c][1] == c) {
-                values[r][c] = tokens[3];
-            } else {
-                values[parents[r][c][0]][parents[r][c][1]] = tokens[3];
-            }
+            values[parents[r][c][0]][parents[r][c][1]] = tokens[3];
         } else {
-            String value1 = tokens[1];
-            String value2 = tokens[2];
-
             for (int i = 1; i <= 50; i++) {
                 for (int j = 1; j <= 50; j++) {
-                    if (values[i][j] == value1) {
-                        values[i][j] = value2;
+                    if (values[parents[i][j][0]][parents[i][j][1]].equals(tokens[1])) {
+                        values[parents[i][j][0]][parents[i][j][1]] = tokens[2];
                     }
                 }
             }
@@ -124,17 +125,16 @@ public class Solution {
     public void print(List<String> result, String[] tokens) {
         int r = Integer.parseInt(tokens[1]);
         int c = Integer.parseInt(tokens[2]);
-        String value = values[parents[r][c][0]][parents[r][c][1]] == "" ?
+        String value = values[parents[r][c][0]][parents[r][c][1]].equals("") ?
                 "EMPTY" :
                 values[parents[r][c][0]][parents[r][c][1]];
         result.add(value);
     }
 
 
-
-     public static void main(String[] args) {
-         Solution sol = new Solution();
-//         System.out.println(Arrays.toString(sol.solution(new String[]{"UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap", "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean", "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian", "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik", "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4"})));
-         System.out.println(Arrays.toString(sol.solution(new String[]{"UPDATE 1 1 a", "UPDATE 1 2 b", "UPDATE 2 1 c", "UPDATE 2 2 d", "MERGE 1 1 1 2", "MERGE 2 2 2 1", "MERGE 2 1 1 1", "PRINT 1 1", "UNMERGE 2 2", "PRINT 1 1"})));
-     }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.solution(new String[]{"UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap", "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean", "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian", "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik", "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4"})));
+        System.out.println(Arrays.toString(sol.solution(new String[]{"UPDATE 1 1 a", "UPDATE 1 2 b", "UPDATE 2 1 c", "UPDATE 2 2 d", "MERGE 1 1 1 2", "MERGE 2 2 2 1", "MERGE 2 1 1 1", "PRINT 1 1", "UNMERGE 2 2", "PRINT 1 1"})));
+    }
 }
