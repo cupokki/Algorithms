9970aca0b7a05de8488b0aa4a081f388728afcb4
cupokki <nd1346@gmail.com>
2026-07-13 23:42:43 +0900

    temp

--- src/main/java/programmers/n60063/Solution.java
+++ src/main/java/programmers/n60063/Solution.java
@@ -1,6 +1,7 @@
 package programmers.n60063;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Solution {
     /*
@@ -9,82 +10,58 @@ public class Solution {
     로봇은 벽이나 지도 밖으로 이동 할 수 없다.
     로봇은 회전가능, 상하좌우 이동가능
     */
-    int h, w;
-    int[][] map;
     int[] dr = {0, -1, 0, 1};
     int[] dc = {1, 0, -1, 0};
 
+    int[] rr = {0, -1, 0, 1}; // rotate r
+    int[] rc = {1, 0, -1, 0}; // rotate c
+
+    int h, w;
+    int[][] map;
     public int solution(int[][] board) {
         int answer = 0;
         h = board.length;
         w = board[0].length;
         map = board;
+        Queue<int[]> q = new LinkedList<>(); // r, c, wingDir, cost
 
-        Queue<int[]> q = new LinkedList<>(); // r, c, wingDir, dist
-
-        boolean[][][] visited = new boolean[board.length][board[0].length][4];
-        q.offer(new int[]{0, 0, 0, 0});
+        boolean[][][] visited = new boolean[h][w][2];
+        q.offer(new int[]{0, 0, 0, 1, 0});
         visited[0][0][0] = true;
 
         while (!q.isEmpty()) {
             int[] state = q.poll();
-            int r = state[0], c = state[1]; // 피벗 좌표
-            int wingDir = state[2]; // 윙 방향
-            int dist = state[3]; // 이동 거리
+            int direction = state[3];
+            int cost = state[4];
+            int nr1, nc1, nr2, nc2;
 
-            // 회전;
-            for (int i = 1; i < 4; i++) {
-                int nw = (wingDir + i) % 4;
-                int wr = r + dr[nw];
-                int wc = c + dc[nw];
-//                if () continue;// 회전 장애물 확인
-                if (visited[r][c][nw]) continue;
-                if (wr < 0 || wc < 0 || wr >= h || wc >= w) continue;
-                if (board[wr][wc] == 0) {
-                    visited[r][c][nw] = true;
-                    q.offer(new int[]{r, c, nw, dist + 1});
-                    if (wr == h - 1 && wc == w - 1) {
-                        return dist + 1;
-                    }
-                }
-            }
+            // vertical to horizontal
 
-            // 이동
+            // horizontal to vertical
+
+            // move
             for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                int nwr = nr + dr[wingDir];
-                int nwc = nc + dc[wingDir];
-
-                if (nr < 0 || nc < 0 || nr >= h || nc >= w) continue;
-                if (nwr < 0 || nwc < 0 || nwr >= h || nwc >= w) continue;
-                if (board[nr][nc] == 1 || board[nwr][nwc] == 1) continue;
-                if (!visited[nr][nc][wingDir]) {
-                    visited[nr][nc][wingDir] = true;
-                    q.offer(new int[]{nr, nc, wingDir, dist + 1});
-                    if ((nr == h - 1 && nc == w - 1) || (nwr == h - 1 && nwc == w - 1)) {
-                        return dist + 1;
-                    }
+                nr1 = state[0] + dr[i]; nc1 = state[1] + dc[i];
+                nr2 = state[2] + dr[direction]; nc2 = state[3] + dc[direction];
+                if(isValid(nr1, nc1, nr2, nc1) && !visited[nr1][nc1][direction]) {
+                    visited[nr1][nc1][direction] = true;
+                    q.offer(new int[]{nr1, nc1, nr2, nc2, cost + 1});
                 }
-
             }
         }
         return -1;
     }
-
-    public boolean validPos(int r, int c, int wingDir) {
-        // 피벗 유효한지
-        if (r < 0 || c < 0 || r >= h || c >= w) return false;
-        if (map[r][c] == 1) return false;
-
-        // 피벗이 아닌 부위가 범위 유효한지.
-        int wr = r + dr[wingDir];
-        int wc = c + dc[wingDir];
-        if (wr < 0 || wc < 0 || wr >= h || wc >= w) return false;
-        if (map[wr][wc] == 1) return false;
+    public boolean isValid(int r1, int c1, int r2, int c2) {
+        if (r1 < 0 || c1 < 0 || r1 >= h || c1 >= w) return false;
+        if (r2 < 0 || c2 < 0 || r2 >= h || c2 >= w) return false;
+        if (map[r1][c1] == 1 || map[r2][c2] == 1) return false;
 
         return true;
+    }
+    public boolean isGoal(int r1, int c1, int r2, int c2) {
+        if (r1 == h - 1 && c1 == w - 1) return true;
+        if (r2 == h - 1 && c2 == w - 1) return true;
+        return false;
     }
 
     public static void main(String[] args) {
