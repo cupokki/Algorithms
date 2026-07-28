e663aaecca3cd481792e0945cd8345cde4e97140
cupokki <nd1346@gmail.com>
2026-07-28 23:59:20 +0900

    temp

--- src/main/java/programmers/n60060/Solution.java
+++ src/main/java/programmers/n60060/Solution.java
@@ -25,41 +25,50 @@ public class Solution {
         int[] answer = new int[queries.length];
 
         Arrays.sort(words);
+        // 역순정렬한 걸로 탐색?
+        String[] reversedWords = new String[words.length];
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder sb = new StringBuilder(words[i]);
+            reversedWords[i] = sb.reverse().toString();
+        }
+        Arrays.sort(words);
 
 //        ab abc abb, aba, acc
         // ab?? -> ab, abc
 
         for (int i = 0; i < queries.length; i++) {
+            String[] curWords;
             String keyword = queries[i];
             int matchCnt = 0;
             int start = 0, end = keyword.length();
             if (keyword.charAt(0) == '?') { // 앞부터 ?임
-                while (keyword.charAt(start) == '?') {
-                    start++;
-                }
+                curWords = reversedWords;
+                keyword = new StringBuilder(queries[i]).reverse().toString();
             } else { // 뒤 부터 ?임
-                while (keyword.charAt(end - 1) == '?') {
-                    end--;
-                }
+                curWords = words;
+            }
+            while (keyword.charAt(end - 1) == '?') {
+                end++;
             }
 
             // 와일드카드와 일치하는 words 원소의 인덱스 발견
-            int idx = binarySearch(words, keyword, 0, words.length, start, end);
+            int idx = binarySearch(curWords, keyword, 0, curWords.length, start, end);
+            if (idx != -1) {
+                // 역순으로 더있는지 확인
+                for (int j = idx; j >= 0; j--) {
+                    // 길이가 다르거나, 대상 구간 문자가 일치하지않으면
+                    if (curWords[j].length() != keyword.length()) break;
+                    if (compare(curWords[j], keyword, start, end) != 0) break;
+                    matchCnt++;
+                }
 
-            // 역순으로 더있는지 확인
-            for (int j = idx; j >= 0; j--) {
-                // 길이가 다르거나, 대상 구간 문자가 일치하지않으면
-                if (words[j].length() != keyword.length()) break;
-                if (compare(words[j], keyword, start, end) != 0) break;
-                matchCnt++;
-            }
-
-            // 정방향으로 더 있는지 확인
-            for (int j = idx; j < words.length; j++) {
-                // 길이가 다르거나, 대상 구간 문자가 일치하지않으면
-                if (words[j].length() != keyword.length()) break;
-                if (compare(words[j], keyword, start, end) != 0) break;
-                matchCnt++;
+                // 정방향으로 더 있는지 확인
+                for (int j = idx + 1; j < curWords.length; j++) {
+                    // 길이가 다르거나, 대상 구간 문자가 일치하지않으면
+                    if (curWords[j].length() != keyword.length()) break;
+                    if (compare(curWords[j], keyword, start, end) != 0) break;
+                    matchCnt++;
+                }
             }
 
             answer[i] = matchCnt;
@@ -70,7 +79,6 @@ public class Solution {
 
     int compare(String word, String keyword, int start, int end) {
         for (int i = start; i < end; i++) {
-            // 그냥 여기서 ???을 처리하게 해도 되겠군
             if (word.charAt(i) != keyword.charAt(i)) {
                 return word.charAt(i) - keyword.charAt(i);
             }
@@ -80,28 +88,28 @@ public class Solution {
 
 
     int binarySearch (String[] words, String keyword, int l, int r, int start, int end) {
-        int m = (l + r) / 2;
+        int m = 0;
         while (l < r) {
+            m = (l + r) / 2;
             // m번째 문자가 키워드 보다 큼
             int diff = compare(words[m], keyword, start, end);
             if (diff == 0) {
-                break;
-            }if (diff > 0) {
-                r = m;
-                //
+                return m;
+            } else if (diff < 0) {
+                l = m + 1;
             } else {
-                l = m;
+                r = m - 1;
             }
         }
-        return m;
+        return -1;
     }
 
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.solution(
+        System.out.println(Arrays.toString(sol.solution(
                 new String[]{"frodo", "front", "frost", "frozen", "frame", "kakao"}, new String[]{"fro??", "????o", "fr???", "fro???", "pro?"}
-        ).toString());
+        )));
         // 3, 2, 4, 1, 0
     }
 
