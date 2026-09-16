1b96c47ce1d355b752b858fc7c7d38db8230e38d
cupokki <nd1346@gmail.com>
2026-09-16 23:56:00 +0900

    temp

--- src/main/java/programmers/n17676/Solution.java
+++ src/main/java/programmers/n17676/Solution.java
@@ -1,74 +1,75 @@
 package programmers.n17676;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.*;
+import java.time.*;
 
 public class Solution {
     /*
-    서버증설, 작년 9월 15일 로그를 분석해 최대 TPS 계산한다.
-    최대 TPS: 응답 여부와 관계없이 임의 시간부터 1초간 처리하는 요청의 최대 개수
-        *TPS 산정시 닫힌구간으로 카운팅한다.
-    
-    - 로그 문자열 마다 응답완료시간s, 처리시간t가 공백으로 구분되어 있다.
-    - t: 3이하의 실수
-    - 로그 문자열 lines: s기준 오름차순 정렬되어있다. 최대 2000개의 로그가 있다.
-    
-    => 날짜정보는 필요없다.
-    
-    하루는 몇초인가. -> 24 * 60 * 60 = 3600 * 24 = 74400초
-    2000 * 74400초 충분히 가능하지않나?
+    기준시작
+    기준끝
 
+    현재로그시작
+    현재로그끝
+
+
+    현재로그의 끝이 구간안(end 이전)에 포함되어야한다.
+        ->
+
+    현재로그의 끝이 기준시작을 지났다면, i++
     */
-    class Log {
-        LocalTime s;
-        Duration t;
-    }
-
     public int solution(String[] lines) {
         int answer = 0;
 
-        Queue<Log> q = new LinkedList<>();
-
-
-//         for (int i = 0; i < lines.length; i++) {
-//             String[] tokens = lines[i].split(" ");
-//             LocalTime s = DateTime.parse(tokens[1]);
-//             Duration t = Double.valueOf(tokens[2].substring(tokens[2].length - 1));
-//             q.offer(new Log(s, t));
-//         }
-
         int n = lines.length;
-        Log cur;
+
         for (int i = 0; i < n; i++) {
+
+            int tps = 0;
+
             String[] tokens = lines[i].split(" ");
-            LocalTime start = LocalTime.parse(tokens[1]);
-            LocalTime end = start.plusSeconds(1);
 
-            int cnt = 1;
+            // TPS 측정 구간
+            LocalTime end = LocalTime.parse(tokens[1]);
+            LocalTime start = end.minusSeconds(1).plusNanos(1_000_000L);
 
-            for (int j = i + 1; j < n; j++) {
-
+            for (int j = i; j < n; j++) {
                 tokens = lines[j].split(" ");
-                LocalTime curStart = LocalTime.parse(tokens[1]);
-                Double t = Double.valueOf(tokens[2].substring(tokens[2].length() - 1));
-                LocalTime curEnd = start.plusNanos((long) (t * 1_000_000L));
+                LocalTime responseTime = LocalTime.parse(tokens[1]);
+                double t = Double.valueOf(tokens[2].substring(0, tokens[2].length() - 1)); // duration
+                LocalTime requestTime = responseTime.minusNanos((long)((t * 1_000_000_000L) - 1_000_000L));
 
+                if (responseTime.isAfter(start) || requestTime.isBefore(end)) {
+                    tps++;
+                }
+
+                if (responseTime.isBefore(start)) {
+                    i++;
+                }
             }
 
-            answer = Math.max(cnt, answer);
-
-
+            answer = Math.max(tps, answer);
         }
+
 
         return answer;
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.solution(new String[]{"2016-09-15 01:00:04.001 2.0s", "2016-09-15 01:00:07.000 2s"})); // 1
-        System.out.println(sol.solution(new String[]{"2016-09-15 01:00:04.002 2.0s", "2016-09-15 01:00:07.000 2s"})); // 2
-        System.out.println(sol.solution(new String[]{"2016-09-15 20:59:57.421 0.351s", "2016-09-15 20:59:58.233 1.181s", "2016-09-15 20:59:58.299 0.8s", "2016-09-15 20:59:58.688 1.041s", "2016-09-15 20:59:59.591 1.412s", "2016-09-15 21:00:00.464 1.466s", "2016-09-15 21:00:00.741 1.581s", "2016-09-15 21:00:00.748 2.31s","2016-09-15 21:00:00.966 0.381s", "2016-09-15 21:00:02.066 2.62s"})); // 7
+//        System.out.println(sol.solution(new String[] {"2016-09-15 01:00:04.001 2.0s", "2016-09-15 01:00:07.000 2s"})); // 1
+        System.out.println(sol.solution(new String[] {"2016-09-15 01:00:04.002 2.0s","2016-09-15 01:00:07.000 2s"})); // 2
+        System.out.println(sol.solution(new String[] {"2016-09-15 20:59:57.421 0.351s",
+                "2016-09-15 20:59:58.233 1.181s",
+                "2016-09-15 20:59:58.299 0.8s",
+                "2016-09-15 20:59:58.688 1.041s",
+                "2016-09-15 20:59:59.591 1.412s",
+                "2016-09-15 21:00:00.464 1.466s",
+                "2016-09-15 21:00:00.741 1.581s",
+                "2016-09-15 21:00:00.748 2.31s",
+                "2016-09-15 21:00:00.966 0.381s",
+                "2016-09-15 21:00:02.066 2.62s"})); // 7
     }
+
+
+
 }
