1279a76ea87be779b4951568de149d1536b6c5e0
cupokki <nd1346@gmail.com>
2026-09-24 23:54:05 +0900

    temp

--- src/main/java/programmers/n133500/Solution.java
+++ src/main/java/programmers/n133500/Solution.java
@@ -1,5 +1,7 @@
 package programmers.n133500;
 
+import java.util.*;
+
 public class Solution {
     /*
     - 번호가 매겨진 n개의 등대 (n은 10만 이하의 자연수)
@@ -13,28 +15,48 @@ public class Solution {
     public int solution(int n, int[][] lighthouse) {
         int answer = 0;
 
-        boolean[] turnOnIdx = new boolean[n];
-        boolean[][] graph = new boolean[n][n];
-
-
+        List<Integer>[] nodes = new List[n];
         for (int i = 0; i < n; i++) {
-            int cnt = 0;
-            for (int j = i + 1; j < n; j++) {
-                if (graph[i][j]) cnt++;
-            }
-
-            if (cnt == 1) {
-
-                for (int j = 0; j < n; j++)  if (graph[i][j]) turnOnIdx[j] = true;
-            }
+            nodes[i] = new ArrayList<>();
         }
 
+        for (int i = 0; i < lighthouse.length; i++) {
+            int u = lighthouse[i][0] - 1;
+            int v = lighthouse[i][1] - 1;
 
+            nodes[u].add(v); // 문제조건상 중복된 간선이 주어질 수 없다.
+            nodes[v].add(u);
+        }
+
+        boolean[] covered = new boolean[n];
+        for (int u = 0; u < n; u++) {
+
+            int uDegree = nodes[u].size();
+
+            if (covered[u] || uDegree == 1) continue; // 커버된곳과 말단은 건너뛴다.
+
+            for (int v : nodes[u]) {
+                if (covered[v]) uDegree --;
+            }
+
+            if (uDegree == 1) { // 말단노드이다.
+                int v = nodes[u].iterator().next();
+                if (!covered[v]) { // 부모노드 커버되지않았다면
+                    covered[v] = true;
+                    for (int children : nodes[v]) {
+                        covered[children] = true;
+                    }
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
+//        System.out.println(sol.solution(6, new int[][]{{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}})); // 3
+//        System.out.println(sol.solution(7, new int[][]{{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}})); // 3
         System.out.println(sol.solution(8, new int[][]{{1, 2}, {1, 3}, {1, 4}, {1, 5}, {5, 6}, {5, 7}, {5, 8}})); // 2
         System.out.println(sol.solution(10, new int[][]{{4, 1}, {5, 1}, {5, 6}, {7, 6}, {1, 2}, {1, 3}, {6, 8}, {2, 9}, {9, 10}})); // 3
 
